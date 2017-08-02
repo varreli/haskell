e1569693419b51196442a20ef8157afb8d105971
varreli <varreli@gmail.com>
2017-08-01 21:54:30 -0700

    Clean up home directories

--- palindrome.hs
+++ palindrome.hs
@@ -1,9 +1,13 @@
+palindrome :: Eq a => [a] -> Bool
 palindrome x = x == (reverse x)
 
--- prelude> :t palindrome
--- palindrome :: Eq a => [a] -> Bool
 
+palindrome2 :: Eq a => [a] -> [a] -> Bool
 palindrome2 a b = a == (reverse b)
 
--- prelude> :t palindrome2
--- palindrome2 :: Eq a => [a] -> [a] -> Bool .
+
+-- James Joyce :
+-- "able was I ere I saw elba"
+
+-- "wonder if sununu's fired now"
+-- "wonderifsununusfirednow"
