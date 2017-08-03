36eaca541d32f123c533744b0a1337ba65fa5028
John Werner <varreli@gmail.com>
2017-08-02 19:47:27 -0700

    Add copious notes

--- interpreter.hs
+++ interpreter.hs
@@ -1,6 +1,5 @@
 f :: (a, b) -> (c, d) -> ((b, d), (a, c))
 f x y = ((snd x, snd y), (fst x, fst y))
 
-
-f :: (t1, b1) -> (t, b) -> ((b1, b), (t1, t))
-f x y = ((snd x, snd y), (fst x, fst y))
+-- actual output :
+-- f :: (t1, b1) -> (t, b) -> ((b1, b), (t1, t))
