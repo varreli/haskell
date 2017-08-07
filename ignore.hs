a1760c49f6856e3d14a1974f8da6a558e9a5326e
John Werner <varreli@gmail.com>
2017-08-06 17:58:25 -0700

    Add ignore

--- ignore.hs
+++
@@ -1,4 +0,0 @@
-a :: (a -> c) -> a -> a
-a f x = x 
-
--- basically ignore the first argument
---
+++ study/best/ignore.hs
@@ -0,0 +1,18 @@
+
+agg :: (a -> c) -> a -> a  -- more complex type
+fug :: (a -> a) -> a -> a  -- than inferred.
+fot :: a -> b -> b        
+
+
+agg f x = x
+fug f x = x
+fot f x = x
+
+
+-- ignore the first argument.
+-- (a -> c) don't need to be different types
+-- but they can be. The function *does* need
+-- the types of its arguments to be related,
+-- even if one of those arguments is not used.
+
+-- we could have (a -> d) in first argument.
