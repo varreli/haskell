3531df5a5cb8015639f6c831295495d3118451bc
John Werner <varreli@gmail.com>
2017-08-10 12:29:37 -0700

    Add trivial and eq_study

---
+++ printing/eq_study.hs
@@ -0,0 +1,23 @@
+-- ff is written specific to String here :
+
+ff :: [Char] -> [Char] -> Bool 
+ff a b = (==) a b
+
+main :: IO ()
+main = print (ff "Fish" "Rock")
+
+
+-- ff is a function that is called on a b. 
+-- Adding a where clause is effectively 
+-- changing the variable names to s t  :
+
+-- ff :: [Char] -> [Char] -> Bool 
+-- ff s t = (==) a b
+--   where
+--     a = "Fish"
+--     b = "Rock"
+
+-- main :: IO ()
+-- main = print ff
+
+-- This is wrong
