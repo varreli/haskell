99429be6339bddf102ec4284df678950be01f8de
John Werner <varreli@gmail.com>
2017-08-10 19:55:24 -0700

    Days of week

---
+++ days.hs
@@ -0,0 +1,22 @@
+data DayOfWeek = 
+  Mon | Tue | Weds | Thu | Fri | Sat | Sun
+
+data Date =
+  Date DayOfWeek Int
+
+instance Eq DayOfWeek where
+  (==) Mon Mon   = True
+  (==) Tue Tue   = True
+  (==) Weds Weds = True
+  (==) Thu Thu   = True
+  (==) Fri Fri   = True
+  (==) Sat Sat   = True
+  (==) Sun Sun   = True
+  (==) _ _       = False
+
+instance Eq Date where
+  (==) (Date weekday dayOfMonth)
+       (Date weekday' dayOfMonth') =
+    weekday == weekday' && dayOfMonth == dayOfMonth'
+
+
