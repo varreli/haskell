6087405666fb4a4a5d6e7c38e4885745973aae8c
varreli <varreli@gmail.com>
2017-08-14 21:55:37 -0700

    Synch again

--- 4_chap6.hs
+++ 4_chap6.hs
@@ -7,16 +7,16 @@ instance Eq a => Eq (Which a) where
 
 -- Pattern matching is what brings it to the
 -- ignore case here.
--- If the eq instance is "passed"   
--- ThisOne 5 == ThatOne 5, what will happen 
--- is that first: You have the line 
--- (==) (ThisOne a) (ThisOne b); you have a 
--- ThatOne in the second parameter so it 
+-- If the eq instance is "passed"
+-- ThisOne 5 == ThatOne 5, what will happen
+-- is that first: You have the line
+-- (==) (ThisOne a) (ThisOne b); you have a
+-- ThatOne in the second parameter so it
 -- doesn't match and it tries the second line.
 
--- The second line has a ThatOne as the first 
--- parameter and so it doesn't match the 
--- comparison you "passed in", so it goes to 
--- the third line. The third line says "return 
--- False for anything you give me" so it 
--- returns false.
+-- The second line has a ThatOne as the first
+-- parameter and so it doesn't match the
+-- comparison you "passed in", so it goes to
+-- the third line. The third line says "return
+-- False for anything you give me" so it
+-- returns False.
