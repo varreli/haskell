e9d9bc35825c6147ef97bcc600ab85866a32d07a
John Werner <varreli@gmail.com>
2017-08-14 19:45:01 -0700

    Upgrade beautiful commitment_$

--- 4_chap6.hs
+++ 4_chap6.hs
@@ -5,3 +5,18 @@ instance Eq a => Eq (Which a) where
   (==) (ThatOne a) (ThatOne b) = a == b
   (==) _ _ = False
 
+-- Pattern matching is what brings it to the
+-- ignore case here.
+-- If the eq instance is "passed"   
+-- ThisOne 5 == ThatOne 5, what will happen 
+-- is that first: You have the line 
+-- (==) (ThisOne a) (ThisOne b); you have a 
+-- ThatOne in the second parameter so it 
+-- doesn't match and it tries the second line.
+
+-- The second line has a ThatOne as the first 
+-- parameter and so it doesn't match the 
+-- comparison you "passed in", so it goes to 
+-- the third line. The third line says "return 
+-- False for anything you give me" so it 
+-- returns false.
