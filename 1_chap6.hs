3fddeb745230a2b4ebe7e6570e507a777764416e
varreli <varreli@gmail.com>
2017-08-14 22:24:08 -0700

    Fix 1_chap6.hs.

--- 1_chap6.hs
+++ 1_chap6.hs
@@ -3,14 +3,14 @@ data TisAnInteger = TisAn Integer deriving Show
 instance Eq TisAnInteger where
   (==) (TisAn a) (TisAn b) = a == b
 
-
-
+-- ------------------------------------------------ 
 data TwoIntegers = Two Integer Integer deriving Show
 
 instance Eq TwoIntegers where
   (==) (Two r s) (Two r' s') =
     r == r' && s == s'
 
+-- ------------------------------------------------
 data StringOrInt = TisAnInt Int | TisAString String
 
 instance Eq StringOrInt where
