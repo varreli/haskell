ed0ef9d97039faecfd1eb10ae5eb532c37685739
varreli <varreli@gmail.com>
2017-08-18 12:52:44 -0700

    Edit

--- study/best/sign_identity2.hs
+++ study/best/sign_identity2.hs
@@ -16,11 +16,11 @@ xyl x y = x / y
 -- λ > :t xyl id 4 5
 -- xyl id 4 5 :: (Num a, Fractional (a -> a)) => a
 
--- This needs (Num a .. ) so that it can convert the 
+-- This needs (Num a .. ) so that it can convert the
 -- literal 5 to an 'a'
 
---λ > :t xyl id 4
---xyl id 4 :: Fractional (a -> a) => a -> a
+-- λ > :t xyl id 4
+-- xyl id 4 :: Fractional (a -> a) => a -> a
 
 -- 'xyl id 4' applies xyl to id; it then applies
 -- the result of that to 4.
