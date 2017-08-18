1c23b296bbfd12dde9b5a09f794ae5aed841fcc0
varreli <varreli@gmail.com>
2017-08-17 22:25:38 -0700

    Synch

--- study/best/bestDay.hs
+++ study/best/bestDay.hs
@@ -1,8 +1,10 @@
-data DayOfWeek = Mon | Tue | Weds | Thu | Fri | Sat | Sun deriving ( Eq, Ord, Show)
+data DayOfWeek = Mon | Tue | Weds | Thu | Fri | Sat
+  | Sun deriving ( Eq, Ord, Show)
 
 
-data BestDayOfWeek = Monday | Tuesday | Wednesday | Thursday
-  | Friday | Saturday | Sunday deriving (Eq, Show)
+data BestDayOfWeek = Monday | Tuesday | Wednesday
+  | Thursday | Friday | Saturday | Sunday
+    deriving (Eq, Show)
 
 instance Ord BestDayOfWeek where
 
@@ -10,3 +12,7 @@ instance Ord BestDayOfWeek where
   compare Friday _      = GT
   compare _ Friday      = LT
   compare _ _           = EQ
+
+  -- deriving does not introduce constrictions.
+  -- It instructs the compiler to make standard
+  -- instances for you.
