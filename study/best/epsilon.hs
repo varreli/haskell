9d6404564d35bbe395e88053dc16bc6478931c24
varreli <varreli@gmail.com>
2017-08-21 17:06:12 -0700

    Edit epsilon

--- study/best/epsilon.hs
+++ study/best/epsilon.hs
@@ -16,6 +16,14 @@ numbers xs = [x | x <- xs , even x]
 -- from xs and x is even."  ','  corresponds to
 -- 'and' . 'and' is used to add conditions
 -- beyond mere membership in the list being
--- drawn from.
+-- drawn from. Study conditions here :
 
 set = [ y * 2 | y <- [1..10], y * 2 >= 14]
+--  >> [14,16,18,20]
+set = [ y * 2 | y <- [1..10], y >= 14 ]
+--  >> []
+
+listTimes3 = [ x * 3 | x <- [1..10], x * 3 <= 50]
+-- >> [3,6,9,12,15,18,21,24,27,30]
+listTimes3 = [x * 3 | x <- [1..10], x  <= 50]
+-- >> [3,6,9,12,15,18,21,24,27,30]
