943f388dfb0c9d48d6569e5494ccaca0a29fa16b
John Werner <varreli@gmail.com>
2017-08-24 12:41:05 -0700

    Nada added

---
+++ pb.hs
@@ -0,0 +1,11 @@
+newtype Nada = Nada Double 
+  deriving (Eq, Show, Num, Fractional)
+
+instance Fractional Nada where
+  (Nada x) / (Nada y) = Nada (x / y)
+  recip (Nada n) = Nada (recip n)
+  fromRational r = Nada (fromRational r)
+
+-- newtype is basically(*) like data, except 
+-- only a single data constructor is allowed - 
+-- further, it must take exactly one argument.
