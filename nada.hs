58f08fe2e7681f2cbc6985ce78daf61df0b18b9d
John Werner <varreli@gmail.com>
2017-08-24 17:42:45 -0700

    Clean up directories

--- nada.hs
+++
@@ -1,9 +0,0 @@
-{-# Language GeneralizedNewtypeDeriving #-} 
-
-newtype Nada = Nada Double 
-  deriving (Eq, Show, Num, Fractional)
-
-instance Fractional Nada where
-  (Nada x) / (Nada y) = Nada (x / y)
-  recip (Nada n) = Nada (recip n)
-  fromRational r = Nada (fromRational r)
