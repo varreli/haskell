97137fbb31cdd4e30eff7be22a8d4e5e9f013df4
John Werner <varreli@gmail.com>
2017-09-08 19:58:22 -0700

    Synch

--- boss.hs
+++ boss.hs
@@ -8,9 +8,13 @@ reportBoss :: Employee -> Employee -> IO ()
 reportBoss e e' = 
   putStrLn $ show e ++ " is boss of " ++ show e'
 
-employeeRank :: Employee -> Employee -> IO ()
-employeeRank e e' =
-  case compare e e' of
+employeeRank :: (Employee -> Employee -> Ordering) 
+                -> Employee
+                -> Employee 
+                -> IO ()
+
+employeeRank f e e' =
+  case f e e' of
     GT -> reportBoss e e'
     EQ -> putStrLn "Neither is the boss"
     LT -> (flip reportBoss) e e'
