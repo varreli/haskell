cf144d10087b1c7f8bc93f1fdf0db5cc28081ded
John Werner <varreli@gmail.com>
2017-09-09 19:34:12 -0700

    Add fllp

--- gg_const/f_a_c_=_f_a.hs
+++ gg_const/f_a_c_=_f_a.hs
@@ -1,12 +1,27 @@
 gg :: (a -> b) -> a -> c -> b
-gg f a c = f a
-
+-- gg f a c = f a
+gg = \f a c -> f a
 
 -- λ > gg (flip const) 6 7 "qq"
--- "qq"
+-- "qq"                          -- equivalent to:
 -- λ > gg (const id) 3 7 "yy"
 -- "yy"
 
 -- λ > gg (+) 5 7 6
 -- 11
+
+-- λ > ( (\f a c -> f a) const 1 2 "t" )
+-- 1
+
+-- note gg and (+) create a union and the
+-- need of a third parameter :
+
+-- λ > ((\f a c -> f a) (+) 1 2 3 ) 
+-- 4
+
+-- note (not) only takes 1 Bool arg but still
+-- requires two args because f a c -> f a:
+
+-- λ > ((\f a c -> f a) (not) False "t") 
+-- True
 
--- xy.hs
+++
@@ -1,5 +0,0 @@
-nums x =
-  case compare x 0 of
-    LT -> " negative " 
-    GT -> " positive " 
-    EQ -> " zero "
