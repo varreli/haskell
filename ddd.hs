61f671d57a6fb645f98c70f38ea145f2f9ca4f5f
varreli <varreli@gmail.com>
2017-10-03 19:39:08 -0700

    add notes erisco

--- ddd.hs
+++ ddd.hs
@@ -1,9 +1,9 @@
-applyTimes :: (Eq a, Num a) => a -> (b -> b) -> b -> b
+applyTimes :: a -> (b -> b) -> b -> b
 
 applyTimes 0 f b = b
 applyTimes n f b = f . applyTimes (n - 1) f $ b
 
 
-incTimes :: (Eq a, Num a) => a -> a -> a
-incTimes times n = applyTimes times ( + 1 ) n
+-- incTimes :: (Eq a, Num a) => a -> a -> a
+-- incTimes times n = applyTimes times ( + 1 ) n
 
