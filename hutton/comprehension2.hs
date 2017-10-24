e9fabd07cfdeeb1f4c9b69ab5a1c6de80ddd5163
varreli <varreli@gmail.com>
2017-10-23 23:52:45 -0700

    Edits

--- hutton/comprehension2.hs
+++ hutton/comprehension2.hs
@@ -3,3 +3,18 @@ pairs xs = zip xs (tail xs)
 
 sorted :: Ord a => [a] -> Bool
 sorted xs = and [ x <= y | (x, y) <- pairs xs]
+
+
+--  λ > pairs [2,3,4,5,6]
+--  [(2,3),(3,4),(4,5),(5,6)]
+
+
+-- on line 5 'and' returns the conjunction of the 
+-- boolean list:
+
+-- and [True,True,True]
+-- True
+
+
+--  λ > sorted [2,3,4,5,6]
+--  True 
