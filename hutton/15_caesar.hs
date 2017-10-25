fc69f86302670632db6ec21d1e1383a513dde920
varreli <varreli@gmail.com>
2017-10-25 16:38:21 -0700

    Add freqs percent encode count lowers

--- hutton/15_caesar.hs
+++ hutton/15_caesar.hs
@@ -10,3 +10,21 @@ shift :: Int -> Char -> Char
 shift n c 
   | isLower c = int2let (mod (let2int c + n) 26)
   | otherwise = c
+
+encode :: Int -> [Char] -> [Char]
+encode n xs = [shift n x | x <- xs]
+
+
+
+percent :: Int -> Int -> Float
+percent m n = (fromIntegral m / fromIntegral n) * 100
+
+lowers :: [Char] -> Int
+lowers xs = length [ x | x <- xs , x >= 'a' && x <= 'z']
+
+count :: Char -> [Char] -> Int
+count char xs = length [ x | x <- xs , x == char]
+
+freqs :: [Char] -> [Float]
+freqs xs = [ percent (count x xs) n | x <- ['a'..'z'] ]
+  where n = lowers xs
