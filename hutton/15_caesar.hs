83ef6f488eba49dcff591294518e1d61abe876db
John Werner <varreli@gmail.com>
2017-10-26 19:52:48 -0700

    Synch

--- hutton/15_caesar.hs
+++ hutton/15_caesar.hs
@@ -14,8 +14,8 @@ shift n c
 encode :: Int -> [Char] -> [Char]
 encode n xs = [shift n x | x <- xs]
 
-
-
+table :: [Float]
+table = [8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0, 6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
 
 percent :: Int -> Int -> Float
 percent m n = (fromIntegral m / fromIntegral n) * 100
@@ -32,4 +32,7 @@ freqs xs = [ percent (count x xs) n | x <- ['a'..'z'] ]
 
 rotate :: Int -> [Int] -> [Int]
 rotate x xs = drop x xs ++ take x xs
+
+chisqr :: [Float] -> [Float] -> Float
+chisqr os es = sum [((o - e)^2)/e | (o,e) <- zip os es]
 
