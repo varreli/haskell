dee05af4bd8c7644edb948daeceadb198e8c5a18
John Werner <varreli@gmail.com>
2017-10-28 16:00:31 -0700

    Synch.

--- hutton/pb.hs
+++
@@ -1,7 +0,0 @@
-squareNoDiagonal :: Int -> [(Int, Int)]
-squareNoDiagonal n = [ (x,y) | (x,y) <- grid n n , x /= y]
-
--- alternative:
-
-sqrND :: Int -> [(Int, Int)]
-sqrND n = filter (uncurry (/=)) $ grid n n
