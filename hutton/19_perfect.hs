1f20f783502a9d86053ecf51852e1b3885c94865
varreli <varreli@gmail.com>
2017-10-28 22:16:40 -0700

    Add oneGen twoGens

--- hutton/19_perfect.hs
+++ hutton/19_perfect.hs
@@ -5,5 +5,11 @@ perfect :: Int -> [Int]
 perfect nn = [ x | x <- [1..nn] , x == sum (init (factors x)) ]  
 
 
+-- !!! perfect I solved alone ; not factors
 
--- !!! perfect solved alone ; not factors
+
+
+twoGens = [ (x,y) | x <- [1,2], y <- [3,4]]
+
+oneGen  =  concat [[ (x,y) | x <- [1,2]] | y <- [3,4]]
+oneGenn =  concat [[ (x,y) | y <- [3,4]] | x <- [1,2]]
