d477afade611b2aa16c3f977fbe4b6bdac75ae0b
varreli <varreli@gmail.com>
2017-10-30 19:10:03 -0700

    Synch school

--- hutton/20_positFind.hs
+++ hutton/20_positFind.hs
@@ -1,14 +1,22 @@
--- find list of positions of x in the list xs :  
+-- find indices list of x in the list xs :  
 
 positions :: Eq a => a -> [a] -> [Int]
 positions x xs = [i | (elem, i) <- zip xs [0..], elem == x] 
 
 
 
+-- find value of a key:
+
 find :: Eq a => a -> [(a, b)] -> [b]
-find key tup = [ v | (k, v) <- tup, k == key] -- find value
+find key tup = [ v | (k, v) <- tup, k == key]
+
+
 
 -- use same type as positions with find:
 
 findPositOf :: Eq a => a -> [a] -> [Int]
-findPositOf elem xs = find elem (zip xs [0..]) 
+findPositOf val xs = find val (zip xs [0..]) 
+
+
+-- this won't take a tuple of various types 
+-- without a more elaborate type  -  i.e. (1, "boo")
