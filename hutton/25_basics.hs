48cf1b8d2dc451212fec370121154f3b067cb0d3
John Werner <varreli@gmail.com>
2017-11-07 11:05:12 -0800

    Synch basics

--- hutton/25_basics.hs
+++ hutton/25_basics.hs
@@ -10,3 +10,10 @@ facct :: Int -> Int
 facct 0 = 1
 facct x | x > 0 = x * facct (x - 1)
         | otherwise = 0 
+
+
+sumdown :: Int -> Int
+sumdown n
+  | n < 0 = 0
+  | otherwise = n + sumdown (n - 1) 
+
