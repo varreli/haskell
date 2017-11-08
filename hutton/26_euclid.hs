d2793efafb183b2e1688d8c03c5b129366158cc4
John Werner <varreli@gmail.com>
2017-11-08 12:07:28 -0800

    Add trace to euclids

--- hutton/26_euclid.hs
+++ hutton/26_euclid.hs
@@ -1,6 +1,11 @@
+import Debug.Trace
+
 euclid :: Int -> Int -> Int
+euclid s t | traceShow (s, t) False = undefined
 euclid s t
   | s <= 0, t <= 0 = 0
   | s == t = s
   | s < t = euclid s ( t - s )
   | s > t = euclid ( s - t ) t
+
+
