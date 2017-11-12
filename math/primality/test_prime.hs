e4f201bd876561e50e1b5b8ec41f5e62a9535150
John Werner <varreli@gmail.com>
2017-11-12 12:32:28 -0800

    synch hask road

--- math/primality/test_prime.hs
+++ math/primality/test_prime.hs
@@ -1,9 +1,9 @@
 divides :: Integer -> Integer -> Bool
-
 divides d n = rem n d == 0
+
 ldf :: Integer -> Integer -> Integer
 ldf k n     | divides k n = k
-            | k^2 >= n = n
+            | k^2 > n = n
             | otherwise = ldf (k+1) n
 
 ld :: Integer -> Integer
