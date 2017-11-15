e33d7bf69095118e7210eead2db0649e71789481
varreli <varreli@gmail.com>
2017-11-14 17:46:27 -0800

    Synch school

--- hutton/29_recurse.hs
+++ hutton/29_recurse.hs
@@ -4,19 +4,19 @@ concatt :: [[a]] -> [a]
 concatt [] = []
 concatt (xs:xss) = xs ++ concatt xss
 
--- produce a list of n identical elemments:
+-- produce a list of n identical elements:
 
 repllicate :: Int -> a -> [a]
 repllicate 0 _ = []
 repllicate n x = x : repllicate (n - 1) x
 
--- select the nth elemment of a list:
+-- select the nth element of a list:
 
 (!!>) :: [a] -> Int -> a
 (!!>) (x:xs) 0 = x 
 (!!>) (x:xs) n = (!!>) xs (n - 1)
 
--- decide if a value is an elemment of a list:
+-- decide if a value is an element of a list:
 
 elemm :: Eq a => a -> [a] -> Bool
 elemm _ [] = False
