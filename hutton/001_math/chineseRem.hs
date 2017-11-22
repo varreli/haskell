7bcaad51979920c6e2f5815aa805e56f24a749d2
John Werner <varreli@gmail.com>
2017-11-22 12:07:23 -0800

    Add math 001 notes

---
+++ hutton/001_math/chineseRem.hs
@@ -0,0 +1,10 @@
+-- all number theory algorithms ultimately come 
+-- down to Euclid's algorithm
+
+-- re: solutions to chinese remainder theorem
+
+-- x = a1 mod n1, x = a2 mod n2, 
+
+-- So solutions to all congruences will be 
+-- arithmetic sequences with period lcm(n1, n2).
+
--- hutton/001_math/extendEu.hs
+++ hutton/001_math/extendEu.hs
@@ -7,3 +7,7 @@ extendEu a 0 = (1, 0)
 extendEu a b = (t, s - q * t)
   where (q, r) = quotRem a b
         (s, t) = extendEu b r
+
+
+-- all number theory algorithms ultimately come 
+-- down to Euclid's algorithm
