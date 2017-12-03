5e0ce9c9b6a656092bbd8b86ae1befe563784a41
John Werner <varreli@gmail.com>
2017-12-03 14:59:18 -0800

    Add make8 to 50

--- hutton/50_bits2Int.hs
+++ hutton/50_bits2Int.hs
@@ -6,6 +6,15 @@ bits2Int :: [Bit] -> Int
 bits2Int bits = sum [x * y | (x,y) <- zip weights bits] 
     where weights = iterate (* 2) 1
 
+-- > bits2Int [1,0,1,1,0,0,0,0]
+13
 
 bitss2Int :: [Bit] -> Int 
 bitss2Int = foldr (\x y -> x + 2 * y) 0
+
+int2Bits :: Int -> [Bit]
+int2Bits 0 = []
+int2Bits n = mod n 2 : int2Bits (n `div` 2)
+
+make8 :: [Bit] -> [Bit]
+make8 bits = take 8 (bits ++ repeat 0)
