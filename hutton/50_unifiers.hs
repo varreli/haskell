7feccec1cb760c3c2bdb93b55b7e0fa41e60896d
varreli <varreli@gmail.com>
2017-12-03 20:39:18 -0800

    Worked on flip id

--- hutton/50_unifiers.hs
+++ hutton/50_unifiers.hs
@@ -3,8 +3,17 @@
 > :t id
 a -> a
 
+
 > :t flip id
 b -> (b -> c) -> c
+
+-- id is gone after you apply flip to it:
+
+-- > flip id 4 (/) 3
+-- > flip 4 (/) 3
+-- > (/) 4 3
+-- 1.33333
+
 
 > :t const id
 b -> a -> a
