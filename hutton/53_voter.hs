f2bd3b76cd1254d00dad0d7c42c2973afbd563cb
John Werner <varreli@gmail.com>
2017-12-08 11:33:29 -0800

    Add winner to votes

--- hutton/53_voter.hs
+++ hutton/53_voter.hs
@@ -1,5 +1,8 @@
 import Data.List
 
+votes :: [String]
+votes = ["Pete","John","John","John","Pete","Ralph"]
+
 counter :: Eq a => a -> [a] -> Int
 counter x = length . filter (== x)
 
@@ -9,3 +12,6 @@ rmdups (x:xs) = x : filter (/= x) (rmdups xs)
 
 result :: Ord a => [a] -> [(Int, a)]
 result vs = sort [ (counter v vs, v) | v <- rmdups vs ]
+
+winner :: Ord a => [a] -> a
+winner = snd . last . result
