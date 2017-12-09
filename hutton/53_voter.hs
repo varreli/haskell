3eb418a6ea6ba39a3e0a480f37205c3836742d50
John Werner <varreli@gmail.com>
2017-12-09 13:02:23 -0800

    pattern match inside case and idea of refutable

--- hutton/53_voter.hs
+++ hutton/53_voter.hs
@@ -3,12 +3,12 @@ import Data.List
 votes :: [String]
 votes = ["Pete","John","John","John","Pete","Ralph"]
 
-counter :: Eq a => a -> [a] -> Int
-counter x = length . filter (== x)
-
 rmdups :: Eq a => [a] -> [a]
 rmdups [] = []
 rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+
+counter :: Eq a => a -> [a] -> Int
+counter x = length . filter (== x)
 
 result :: Ord a => [a] -> [(Int, a)]
 result vs = sort [ (counter v vs, v) | v <- rmdups vs ]
@@ -17,13 +17,12 @@ winner :: Ord a => [a] -> a
 winner = snd . last . result
 
 
+-- example 2: 
 
 
 ballots :: [[String]]
 ballots = [ ["Pete","John"], ["John", "Ralph", "Pete"], ["John", "Pete"], [], ["John"] ]
 
-
--- remove empty ballots:
 rmEmpty :: Eq a => [[a]] -> [[a]]
 rmEmpty = filter (/= [])
 
@@ -37,5 +36,17 @@ winner' :: Ord a => [[a]] -> a
 winner' bs = case rank (rmEmpty bs) of
   [c] -> c
   (c:cs) -> winner' (elim c bs)
+
+-- [c] is a one element list, the winner, and
+-- returns the winner.
+
+-- note difference between refutable and 
+-- irrefutable:
+
+-- f _ [] = ..    -- the wildcard is irrefutable.
+                  -- it wont fail.
+
+-- f x [] = ..   x creates a binding to a value.
+-- f _ [] = ..   _ does not create a binding.
 
 
