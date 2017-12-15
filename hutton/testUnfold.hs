b01ce3d8d1f42165f3bb64bea8335764b9be6a16
John Werner <varreli@gmail.com>
2017-12-15 11:44:16 -0800

    Binary study with array

---
+++ hutton/testUnfold.hs
@@ -0,0 +1,14 @@
+
+unfold :: (d -> Bool) -> (d -> f) -> (d -> d) -> d -> [f]
+
+unfold p h t x
+  | p x       = []
+  | otherwise = h x : unfold p h t (t x)
+
+
+int2bbin :: Integer -> [Integer] 
+int2bbin = reverse . unfold (== 0) (`mod` 2) (`div` 2)
+
+-- in an array, it's more natural to write 10 as 
+-- [0,1,0,1] because the array indices correspond 
+-- to the powers of two; and 1010b is for human processing.
