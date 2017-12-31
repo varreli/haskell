189fa09086486dbf0e5a414239f066da23c22e23
varreli <varreli@gmail.com>
2017-12-30 18:03:27 -0800

    Add data List with lengthy

--- hutton/68_dataLst.hs
+++ hutton/68_dataLst.hs
@@ -3,3 +3,11 @@ data List a = Nil | Cons a (List a)
 len' :: List a -> Int
 len' Nil = 0
 len' (Cons _ xs) = 1 + len' xs
+
+--  λ > len' (Cons 1  (Cons 1 (Cons 1 (Cons 1 (Cons 1 Nil)))))
+--  5
+
+-- λ > len' (Cons "a"  (Cons "b" (Cons "c" (Cons "d" (Cons "e" Nil)))))
+-- 5
+ 
+
