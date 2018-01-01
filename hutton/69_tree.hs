50ae75aae4bbf52a3c30a78d8db9abd0e206d79a
varreli <varreli@gmail.com>
2017-12-31 20:36:50 -0800

    Synch home

--- hutton/69_tree.hs
+++ hutton/69_tree.hs
@@ -13,3 +13,7 @@ occurs' x (Leaf y)                 = x == y
 occurs' x (Node l y r) | x == y    = True
                        | x < y     = occurs' x l
                        | otherwise = occurs' x r
+
+
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+tt = Leaf (33)
--- hutton/70_tree.hs
+++ hutton/70_tree.hs
@@ -1,3 +1,5 @@
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
 flatten :: Tree a -> [a]
 flatten (Leaf x)     = [x]
 flatten (Node l x r) = flatten l ++ [x] ++ flatten r
@@ -6,7 +8,7 @@ t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
 tt = Leaf (33)
 
 -- > flatten t
--- [1,2,3,4,5,6,7,9]
+-- [1,3,4,5,6,7,9]
 
 -- > flatten tt
 -- [33]
