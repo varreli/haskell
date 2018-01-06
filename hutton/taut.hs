10856470cf46f9b3a1c789c86dd4847af0adc0b9
varreli <varreli@gmail.com>
2018-01-05 18:25:57 -0800

    Add Proposition in taut

--- hutton/taut.hs
+++ hutton/taut.hs
@@ -4,6 +4,10 @@
 -- constructor for each of the 5 possible 
 -- forms of propositions:
 
+
+type Assoc k v = [(k,v)]
+type Subst = Assoc Char Bool
+
 data Prop = Const Bool
           | Var Char
           | Not Prop
@@ -13,3 +17,20 @@ data Prop = Const Bool
 
 -- Const True is a tautology, since:
 -- \x -> True is what Const True returns.
+
+
+-- Truth Tables can be represented as :
+
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A'))
+
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+
+p3 :: Prop
+p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+
+p4 :: Prop
+p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
+
+
