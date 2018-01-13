357f74e094de11dcc4b75731ebfd5dd7a6649445
varreli <varreli@gmail.com>
2018-01-12 22:11:07 -0800

    Worked on update

--- hutton/75_update.hs
+++ hutton/75_update.hs
@@ -1,7 +1,15 @@
+-- compare to boolsR in 74_taut.hs
+
+
+λ > update bss = map (False:) bss ++ map (True:) bss
 
 λ > update [[]]
 [[False, True]]
 
 λ > update (update [[]])]
-[-False,False],[False,True],[True,False],[True,True]
+[False,False],[False,True],[True,False],[True,True]
 
+
+-- recall map 's signature:
+
+map :: (a -> b) -> [a] -> [b]
