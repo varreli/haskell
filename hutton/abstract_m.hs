fc39db4d4514c036232270ac85e18b99ff399b55
John Werner <varreli@gmail.com>
2018-01-22 18:38:45 -0800

    Finish abstract machine

--- hutton/abstract_m.hs
+++ hutton/abstract_m.hs
@@ -1,8 +1,21 @@
 data Expr = Val Int | Add Expr Expr
 
-value :: Expr -> Int
+value' (Val n) = n
+value' (Add x y) = value' x + value' y
 
-value (Val n) = n
-value (Add x y) = value x + value y
+type Cont = [Op]
+data Op = EVAL Expr | ADD Int
+
+exec :: Cont -> Int -> Int
+exec []           n = n
+exec (EVAL y : c) n = eval y (ADD n : c)
+exec (ADD n : c)  m = exec c (n + m)
+
+eval :: Expr -> Cont -> Int
+eval (Val n)   c = exec c n
+eval (Add x y) c = eval x (EVAL y : c)
+
+value :: Expr -> Int
+value e = eval e []
 
 main = print $ value (Add (Add (Val 4) (Val 1)) (Val 10))
