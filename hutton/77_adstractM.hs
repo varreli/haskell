fa3b2bf1b134ee9962bda26fdb39ec5b8b871741
varreli <varreli@gmail.com>
2018-01-24 14:21:23 -0800

    Abst machine

--- hutton/77_adstractM.hs
+++ hutton/77_adstractM.hs
@@ -1,21 +1,30 @@
 data Expr = Val Int | Add Expr Expr
 
-value' (Val n) = n
-value' (Add x y) = value' x + value' y
-
-type Cont = [Op]
+type Control = [Op]
 data Op = EVAL Expr | ADD Int
 
-exec :: Cont -> Int -> Int
+exec :: Control -> Int -> Int
 exec []           n = n
 exec (EVAL y : c) n = eval y (ADD n : c)
 exec (ADD n : c)  m = exec c (n + m)
 
-eval :: Expr -> Cont -> Int
+eval :: Expr -> Control -> Int
 eval (Val n)   c = exec c n
 eval (Add x y) c = eval x (EVAL y : c)
 
 value :: Expr -> Int
 value e = eval e []
 
+
+
+
 -- main = print $ value (Add (Add (Val 4) (Val 1)) (Val 10))
+
+
+
+-- Control represents a stack of things to do (aka a continuation)
+-- after evaluating a subexpression.
+
+-- EVAL and ADD are data constructors.
+
+-- Op and Expr are type constructors.
