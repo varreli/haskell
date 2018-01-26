a70640ec53a9a482fb6f52e6adbc9fe4017b261e
varreli <varreli@gmail.com>
2018-01-25 19:02:58 -0800

    Abst machine

--- hutton/77_abstractM.hs
+++ hutton/77_abstractM.hs
@@ -1,16 +1,16 @@
 data Expr = Val Int | Add Expr Expr
 
-type Control = [Op]
+type Cont = [Op]
 
 data Op = EVAL Expr | ADD Int
 
 
-exec :: Control -> Int -> Int
+exec :: Cont -> Int -> Int
 exec []           n = n
 exec (EVAL y : c) n = eval y (ADD n : c)
 exec (ADD n : c)  m = exec c (n + m)
 
-eval :: Expr -> Control -> Int
+eval :: Expr -> Cont -> Int
 eval (Val n)   c = exec c n
 eval (Add x y) c = eval x (EVAL y : c)
 
@@ -22,15 +22,17 @@ value e = eval e []
 
 
 
--- Control represents a stack of things to do (aka a continuation)
+-- Cont represents a stack of things to do (aka a continuation)
 -- after evaluating a subexpression.
 
 -- EVAL and ADD are data constructors.
 
 -- Op and Expr are type constructors.
 
+-- this is called a proof using constructive induction.
 
--- evaluation :
+
+-- the evaluation order is now explicit:
 
 -- value (Add (Add (Val 4) (Val 1)) (Val 10))
 -- eval (Add (Add (Val 4) (Val 1)) (Val 10)) []
