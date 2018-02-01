7f5916ff13caac7c45bca022ba6f62c0c0fda223
John Werner <varreli@gmail.com>
2018-02-01 11:10:04 -0800

    Synch

---
+++ hutton/80_xy.hs
@@ -0,0 +1,26 @@
+data Ordering = LT | EQ | GT
+compare :: Ord a => a -> a -> Ordering
+
+
+-- using above redefine occurs'      **
+
+
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
+occurs :: Eq a => a -> Tree a -> Bool
+occurs x (Leaf y) = x == y
+occurs x (Node l y r) = x == y || occurs x l || occurs x r
+
+
+
+-- more efficient (checks potentially one branch) :
+
+occurs' :: Ord a => a -> Tree a -> Bool
+occurs' x (Leaf y)                 = x == y
+occurs' x (Node l y r) | x == y    = True
+                       | x < y     = occurs' x l
+                       | otherwise = occurs' x r
+
+
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+tt = Leaf (33)
