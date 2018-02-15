87a3807cc04fea37fb819d9d5efd8fa60f4cac73
John Werner <varreli@gmail.com>
2018-02-14 19:56:43 -0800

    Synch

--- hutton/70_occTree.hs
+++ hutton/70_occTree.hs
@@ -8,17 +8,20 @@ occursT x (Node l y r) =
     LT -> occursT x l
     GT -> occursT x r
 
+
 -- t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Node (Leaf 9) 22 (Leaf 19)))
 
 -- Prelude> occursT 19 t
 -- False
 
-
+-- compare 19 22 -> occursT 19 9 => False
 
 -- Must be in ascending order:
 
--- t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Node (Leaf 9) 19 (Leaf 22)))
+-- t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Node (Leaf 9) 11 (Leaf 19)))
 
 -- Prelude> occursT 19 t
 -- True
+
+-- compare 19 (Leaf 19) => True
 
