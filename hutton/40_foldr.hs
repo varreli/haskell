06819dc56b134b5c2b0fe67dd3a6e22720957007
John Werner <varreli@gmail.com>
2018-04-05 16:45:32 -0700

    Synch

--- hutton/40_foldr.hs
+++ hutton/40_foldr.hs
@@ -1,26 +1,22 @@
-<<<<<<< HEAD
 -- > foldr (:) [5,6,7] [1,2,3,4]
 -- [1,2,3,4,5,6,7]  
 -- ( evaluates to 1 : 2 : 3 : 4 : [5,6,7] )
-=======
+
 lengtth :: [a] -> Int 
 lengtth = foldrr (\_ n -> n + 1) 0
 
 -- foldr iterates over a foldable structure one 
 -- element at a time:   \_   is the 1st element
 -- and n is the accumulator.
->>>>>>> 7485ea4645fd3f90cc47afad7714e1fbc94a1149
-
 
 -- It takes an accumulator function where the first 
 -- parameter is an element from your Foldable 
 -- structure (like a List), and the second parameter 
 -- is your accumulator value.
 
-<<<<<<< HEAD
 (*) -- has both right and left identity: (1*) (*1)
 (+) -- has both right and left identity: (y+) (+y)
-=======
+
 -- We call f with two arguments: x (of type 'a') and 
 -- either v for the empty list case or (foldr f v xs) 
 -- for the non-empty case (both of which are of type 'b') :
@@ -32,7 +28,6 @@ foldrr f v (x:xs) = f x (foldr f v xs)
 -- note: we don't need \x since x is not used:
 
 lengthh = foldrr (\_ n -> n + 1) 0
->>>>>>> 7485ea4645fd3f90cc47afad7714e1fbc94a1149
 
 -- but we need it here:
 
@@ -48,4 +43,5 @@ mathy = foldrr (\x y -> x + y / 2) 54
 
 -- too general; it won't compile; there's no type for 
 -- list [a] : 
+
 matthy = foldr (\x y -> x + y / 2) 54 
