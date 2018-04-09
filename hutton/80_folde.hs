c2ec53055ec797baf2b4240a97724a9d39d98e7a
John Werner <varreli@gmail.com>
2018-04-08 18:44:42 -0700

    Synch 80 folde

--- hutton/80_folde.hs
+++ hutton/80_folde.hs
@@ -1,5 +1,12 @@
+-- Define a higher-order function such that folde f g 
+-- replaces each `Val` constructor in an expression by 
+-- the function `f`, and each `Add` constructor by the 
+-- function `g`.
+
 data Expr = Val Int | Add Expr Expr
 
-folde :: (Int -> a) (a -> a -> a) -> Expr -> a
+folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
 
-folde n f g
+folde f g (Val x) = f x
+folde f g (Add x y) = g (folde f g x) (folde f g y)
+
