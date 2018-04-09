297bcea184f29daaaa745e25a2b1c4ce3456772b
varreli <varreli@gmail.com>
2018-04-08 21:45:20 -0700

    folde eval and size

--- hutton/80_folde.hs
+++ hutton/80_folde.hs
@@ -10,3 +10,13 @@ folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
 folde f g (Val x) = f x
 folde f g (Add x y) = g (folde f g x) (folde f g y)
 
+-- Using folde , define a function eval that evaluates
+-- an expression to an integer value, and a function
+-- size the gives the number of values in an expression.
+
+eval :: Expr -> Int
+eval = folde (+ 0) (+)
+
+size :: Expr -> Int
+size = folde (\_ -> 1) (+)
+
