ae1893415493a1f760725a26d55470fc73329128
varreli <varreli@gmail.com>
2018-04-10 21:17:17 -0700

    Synch Cale help

---
+++ hutton/80_notes.hs
@@ -0,0 +1,45 @@
+-- Cale says:
+
+-- we're taking replacements for each of the data 
+-- constructors, and making those replacements through 
+-- out the structure.
+-- we could write it like:
+
+folde val add = f
+
+where f (Val x) = val x
+
+f (Add x y) = add (f x) (f y)
+
+-- The idea is to replace each Add with add and each Val 
+-- with val throughout the entire structure.
+
+-- So if we have : 
+
+(Add (Val 5) (Add (Val 1) (Val 2)) )
+
+-- the result we would want is:
+
+add (val 5) (add (val 1) (val 2))
+
+
+
+-- Another use example, perhaps we want to know the depth 
+-- of the expression
+
+depth = folde (const 0) (\d1 d2 -> 1 + max d1 d2)
+
+-- Or perhaps we'd just like to flip the entire expression 
+-- over, reversing all the Adds
+
+flipExpr = folde Val (flip Add)
+
+-- that turns  Add (Val 1) (Add (Val 2) (Val 3)) into  Add
+
+(Add (Val 3) (Val 2)) (Val 1)
+
+
+-- As your expressions get more complicated, there will be 
+-- more interesting functions to write...for example, if 
+-- there were lambdas, perhaps we could write something 
+-- to collect up all the free variables
