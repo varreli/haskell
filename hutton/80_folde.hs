eaf638c186f61ed7d9d832dee657d2843dd452ba
John Werner <varreli@gmail.com>
2018-04-10 20:24:31 -0700

    Synch

--- hutton/80_folde.hs
+++ hutton/80_folde.hs
@@ -12,7 +12,7 @@ folde f g (Add x y) = g (folde f g x) (folde f g y)
 
 -- Using folde , define a function eval that evaluates
 -- an expression to an integer value, and a function
--- size the gives the number of values in an expression.
+-- size that gives the number of values in an expression.
 
 eval :: Expr -> Int
 eval = folde (+ 0) (+)
