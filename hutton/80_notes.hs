21adf274f8eb1bc568b6a41b734a416763134b1e
varreli <varreli@gmail.com>
2018-04-11 23:33:11 -0700

    Synch

--- hutton/80_notes.hs
+++ hutton/80_notes.hs
@@ -5,22 +5,26 @@
 -- out the structure.
 -- we could write it like:
 
-folde val add = f
+data Expr = Val Int | Add Expr Expr
 
-where f (Val x) = val x
+folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
 
-f (Add x y) = add (f x) (f y)
+folde f g (Val x) = f x
+folde f g (Add x y) = g (folde f g x) (folde f g y)
+
+folde2 val add = f
+    where f (Val x) = val x
+          f (Add x y) = add (f x) (f y)
 
 -- The idea is to replace each Add with add and each Val 
 -- with val throughout the entire structure.
 
 -- So if we have : 
 
-(Add (Val 5) (Add (Val 1) (Val 2)) )
+tt = folde2 (+ 0) (+) (Add (Val 5) (Add (Val 1) (Val 2)) )
 
 -- the result we would want is:
-
-add (val 5) (add (val 1) (val 2))
+-- add (val 5) (add (val 1) (val 2))
 
 
 
@@ -34,9 +38,8 @@ depth = folde (const 0) (\d1 d2 -> 1 + max d1 d2)
 
 flipExpr = folde Val (flip Add)
 
--- that turns  Add (Val 1) (Add (Val 2) (Val 3)) into  Add
-
-(Add (Val 3) (Val 2)) (Val 1)
+-- that turns  Add (Val 1) (Add (Val 2) (Val 3)) into  
+-- Add (Add (Val 3) (Val 2)) (Val 1)
 
 
 -- As your expressions get more complicated, there will be 
