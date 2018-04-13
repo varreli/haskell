ce2637fb6cf3aa9675fc06377a89430dcd8b8d28
John Werner <varreli@gmail.com>
2018-04-12 19:57:46 -0700

    Synch

--- hutton/81_instances.hs
+++ hutton/81_instances.hs
@@ -1,4 +1,4 @@
--- From the prelude -- complete the instance declarations :
+-- From prelude; complete the instance declarations :
 
 
 instance Eq a => Eq (Maybe a) where
@@ -6,8 +6,17 @@ instance Eq a => Eq (Maybe a) where
   (==) (Just x) (Just y) = x == y
   (==) _ _               = False
 
+-- this will return False:
+-- > (==) Nothing (Just 4)
+
 instance Eq a => Eq [a] where
   (==) [] []         = True
   (==) (x:xs) (y:ys) = x == y && xs == ys
-  (==) _ _           = False
+  otherwise False -- (==) _ _           = False
 
+
+-- the second one is recursive so (==) [] [] 
+-- is a base case. 
+
+-- (==) _ _ = False is "match any other case."  
+-- Note also use: ' otherwise False '
