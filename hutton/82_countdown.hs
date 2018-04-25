7594cec6b794db20a2ab58a1949f8ab785428d0c
John Werner <varreli@gmail.com>
2018-04-24 19:48:23 -0700

    Begin chapt 9

---
+++ hutton/82_countdown.hs
@@ -0,0 +1,29 @@
+data Op = Add | Sub | Mul | Div 
+
+instance Show Op where
+  show Add = "+"
+  show Sub = "-"
+  show Mul = "*"
+  show Div = "/"
+
+valid :: Op -> Int -> Int -> Bool
+valid Add _ _ = True
+valid Sub x y = x > y
+valid Mul _ _ = True
+valid Div x y = mod x y == 0
+
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = div x y
+
+data Expr = Val Int | App Op Expr Expr
+
+instance Show Expr where
+        show (Val n)     = show n
+        show (App o l r) = brak l ++ show o ++ brak r
+          where
+            brak (Val n) = show n
+            brak e       = "(" ++ show e ++ ")"
+
