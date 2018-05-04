589991dfcc6e36dd6ba2eee60dabf39bca8a9848
John Werner <varreli@gmail.com>
2018-05-03 19:50:26 -0700

    synch

--- hutton/82_countdown.hs
+++ hutton/82_countdown.hs
@@ -46,6 +46,11 @@ interleave :: a -> [a] -> [[a]]
 interleave x []     = [[x]]
 interleave x (y:ys) = (x:y:ys) : map (y:) (interleave x ys)
 
+perms :: [a] -> [[a]]
+perms []     = [[]]
+perms (x:xs) = concat (map (interleave x) (perms xs))
+
+
 -- alternative to subs :
 
 subseqs :: [a] -> [[a]]
