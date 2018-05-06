4542f3f24e654a827cde4399acf3e844692395f4
John Werner <varreli@gmail.com>
2018-05-06 15:14:18 -0700

    Add import ShowExpression

--- hutton/82_chapt9/countdown.hs
+++ hutton/82_chapt9/countdown.hs
@@ -1,41 +1,5 @@
-data Op = Add | Sub | Mul | Div 
+import ShowExpressions
 
-instance Show Op where
-  show Add = "+"
-  show Sub = "-"
-  show Mul = "*"
-  show Div = "/"
-
-valid :: Op -> Int -> Int -> Bool
-valid Add _ _ = True
-valid Sub x y = x > y
-valid Mul _ _ = True
-valid Div x y = mod x y == 0
-
-apply :: Op -> Int -> Int -> Int
-apply Add x y = x + y
-apply Sub x y = x - y
-apply Mul x y = x * y
-apply Div x y = div x y
-
-data Expr = Val Int | App Op Expr Expr
-
-instance Show Expr where
-        show (Val n)     = show n
-        show (App o l r) = brak l ++ show o ++ brak r
-          where
-            brak (Val n) = show n
-            brak e       = "(" ++ show e ++ ")"
-
-values :: Expr -> [Int]
-values (Val n) = [n]
-values (App _ l r) = values l ++ values r
-
-eval :: Expr -> [Int]
-eval (Val n)     = [n | n > 0]
-eval (App o l r) = [apply o x y | x <- eval l,
-                                  y <- eval r,
-                                  valid o x y]
 
 subs :: [a] -> [[a]]
 subs []     = [[]]
@@ -62,3 +26,25 @@ solution e nums n = elem (values e) (choices nums)
 -- λ > solution (App Mul (App Add (Val 50) (Val 1)) (App Sub (Val 25) (Val 10))) [1,3,7,10,25,50] 765
 -- True
 
+split :: [a] -> [([a], [a])]
+split []     = []
+split [_]    = []
+split (x:xs) = ([x], xs) : [(x:ls, rs) | (ls, rs) <- split xs]
+
+-- auxillary function combine in exprs:
+
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- ops]
+
+ops :: [Op]
+ops = [Add,Sub,Mul,Div]
+
+--
+
+exprs :: [Int] -> [Expr]
+exprs []  = []
+exprs [n] = [Val n]
+exprs ns  = [ e | (ls, rs) <- split ns,
+                  l        <- exprs ls,
+                  r        <- exprs rs,
+                  e        <- combine l r] 
