d34aa0bf044571073d54292805978ffb22837180
John Werner <varreli@gmail.com>
2018-06-01 19:55:41 -0700

    Synch

--- hutton/82_final9/countdownF.hs
+++ hutton/82_final9/countdownF.hs
@@ -23,12 +23,12 @@ perms (x:xs) = concat (map (interleave x) (perms xs))
 choices :: [a] -> [[a]]
 choices = concat . map perms . subs 
 
+-- variation with $ : 
 -- choices xs = concat . map perms . subs $ xs  
--- -- variation with $
 
 split :: [a] -> [([a], [a])]
 split []     = []
-split [_]    = []                                              -- hangs without thisG
+split [_]    = []                                        --    hangs without this
 split (x:xs) = ([x], xs) : [(x:ls, rs) | (ls, rs) <- split xs]
 
 -- split [1,2,3] = ([1],[2,3]) : (1:[2], [3]) : [(1:2:3[], [])]
@@ -42,6 +42,7 @@ exprs ns = [e | (ls, rs) <- split ns,
                 r        <- exprs rs,
                 e        <- combine l r]
 
+----------------------------------------------------------------
 -- auxillary function combine in exprs:
 
 combine :: Expr -> Expr -> [Expr]
@@ -50,7 +51,7 @@ combine l r = [App o l r | o <- ops]
 ops :: [Op]
 ops = [Add,Sub,Mul,Div]
 
--- auxiliary function for results ------------------------
+-- -- -- auxiliary function for results ------------------------
 
 type Result = (Expr, Int)
 
@@ -71,9 +72,10 @@ results ns  = [res | (ls,rs) <- split ns,
 solve ns n =
   [e | ns' <- choices ns, (e, m) <- results ns', m == n]
 
+-- main :: IO ()
+-- main = print (totalSuccessful [1, 3, 7, 10, 25, 50])
 main :: IO ()
-main = print (totalSuccessful [1, 3, 7, 10, 25, 50])
-
+main = print $ solve [1,3,7,10,25,50] 765
 ----------------------------------------------------------
 
 removeFirst :: Eq a => a -> [a] -> [a]
@@ -88,15 +90,15 @@ isChoice (x:xs) ys = elem x ys && isChoice xs (removeFirst x ys)
 
 ----------------------------------------------------------
 
-possibleExprs :: [Int] -> [Expr]
-possibleExprs = concat . map exprs . choices
-
-successfulExprs :: [Int] -> [[Int]]
-successfulExprs = filter (not . null) . map eval . possibleExprs
-
-totalPossible :: [Int] -> Int
-totalPossible = length . possibleExprs
-
-totalSuccessful :: [Int] -> Int
-totalSuccessful = length . successfulExprs
+-- possibleExprs :: [Int] -> [Expr]
+-- possibleExprs = concat . map exprs . choices
+-- 
+-- successfulExprs :: [Int] -> [[Int]]
+-- successfulExprs = filter (not . null) . map eval . possibleExprs
+-- 
+-- totalPossible :: [Int] -> Int
+-- totalPossible = length . possibleExprs
+-- 
+-- totalSuccessful :: [Int] -> Int
+-- totalSuccessful = length . successfulExprs
 -----------------------------------------------------------
--- math/primality/test_prime.hs
+++ math/primality/test_prime.hs
@@ -14,7 +14,7 @@ primeO n    | n < 1    = error "not a positive integer"
             | n == 1   = False
             | otherwise  = ld n == n
 
-main = print $ map primeO [2011, 2013, 2015, 2017, 2019, 2021, 2023] -- use of map
+-- main = print $ map primeO [2011, 2013, 2015, 2017, 2019, 2021, 2023] -- use of map
 
 -- no need to use = in k^2 >= n = n  .  divides k n = k 
 -- covers k ^ 2 = n , since k AND k^2 would both divide n.
