3a468e377ea8aa495c52069f04d366151f62d1bb
varreli <varreli@gmail.com>
2018-07-05 21:13:43 -0700

    Add notes to max-int.hs

--- not_book/max-int.hs
+++ not_book/max-int.hs
@@ -11,4 +11,11 @@ main = print $ mxmInt [4,22,3,56,77,1]
 -- max a (max b (max c (max d e)))      
 
 
+-- λ > foldl1 max [1,20,4,33,22]
+-- 33
+
+-- λ > scanl1 max [1,20,4,33,22]
+-- [1,20,20,33,33]  
+
 -- see   Debug.SimpleReflect
+
