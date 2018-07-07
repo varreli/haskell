f9b30ec9274e3039ad63830321f0a8cd884d6653
John Werner <varreli@gmail.com>
2018-07-06 19:07:00 -0700

    Synch

--- hutton/83_chapt10/nim_game.hs
+++ hutton/83_chapt10/nim_game.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 next :: Int -> Int
 next 1 = 2
 next 2 = 1
@@ -23,5 +25,23 @@ putRow row num = do putStr (show row)
                     putStr ": "
                     putStrLn (concat (replicate num "* "))
 
+putBoard :: Board -> IO ()
+putBoard [a,b,c,d,e] = do putRow 1 a
+                          putRow 2 b
+                          putRow 3 c
+                          putRow 4 d
+                          putRow 5 e
 
+getDigit :: String -> IO Int
+getDigit prompt = do putStr prompt
+                     x <- getChar
+                     newline
+                     if isDigit x then
+                        return (digitToInt x)
+                     else
+                        do putStrLn "ERROR: Invalid digit"
+                           getDigit prompt
+
+newline :: IO ()
+newline = putChar '\n'
 
--- not_book/max-int.hs
+++ not_book/max-int.hs
@@ -10,7 +10,7 @@ main = print $ mxmInt [4,22,3,56,77,1]
 
 -- max a (max b (max c (max d e)))      
 
--- (max 3 4):5:1:[]
+-- (max 3 4):5:1:[] 
 -- (max 4 5):1:[]
 -- (max 5 1):[]
 -- [5]
