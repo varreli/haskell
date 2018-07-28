56070bdfdae491e87fd340629474f17c36461e3a
varreli <varreli@gmail.com>
2018-07-27 22:08:29 -0700

    Synch file cleanup

---
+++ logic/owlEyes.hs
@@ -0,0 +1,7 @@
+(.).(.) 
+
+-- Here you have a b -> c and an a1 -> a2 -> b, so you can 
+-- get a function a1 -> a2 -> c by supplying the a1 and a2 
+-- to your second argument to get a b, then applying your 
+-- first argument to get a c
+
--- math/pb.hs
+++
@@ -1,7 +0,0 @@
-import Debug.Trace
-
-extendEu' :: Integer -> Integer -> (Integer, Integer)
-extendEu' a b = let y = extendEu a b
-                in y `seq` trace ("extendEu " ++ show a ++ " " ++ show b
-		                                  ++ " = " ++ show y) y
-
