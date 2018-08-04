de90859571a4fa558fb6bb506d9f7b9be762481f
John Werner <varreli@gmail.com>
2018-08-04 11:59:41 -0700

    Add life

---
+++ hutton/83_chapt10/life.hs
@@ -0,0 +1,38 @@
+cls :: IO ()
+cls = putStr "\ESC[2]"
+
+type Pos = (Int, Int)
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do goto p
+                  putStr xs
+
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+width :: Int
+width = 10
+
+height :: Int
+height = 10
+
+type Board = [Pos]
+
+glider :: Board
+glider = [(4,2), (2,3), (4,3), (3,4), (4,4)]
+
+showcells :: Board -> IO ()
+showcells b = sequence_ [writeat p "0" | p <- b]
+
+isAlive :: Board -> Pos -> Bool
+isAlive b p = elem p b
+
+isEmpty :: Board -> Pos -> Bool
+isEmpty b p = not (isAlive b p)
+
+neighbs :: Pos -> [Pos]
+neighbs (x,y) = map wrap [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
+
+wrap :: Pos -> Pos
+wrap (x,y) = ((mod (x-1) width) + 1, (mod (y-1) height) + 1)
+
