cfe47c7da49b84de643eeffc6c8c002a9e83d87c
John Werner <varreli@gmail.com>
2018-09-04 19:16:54 -0700

    Add priceData.hs .

---
+++ data/priceData.hs
@@ -0,0 +1,18 @@
+data Price = Price Integer 
+             deriving (Eq, Show)
+
+data Manufacturer = Mini | Mazda | Tata
+                    deriving (Eq, Show)
+
+data Airline = Papu | JetBlue | United
+               deriving (Eq, Show)
+
+data Vehicle = Car Manufacturer Price
+               | Plane Airline
+               deriving (Eq, Show)
+
+myCar = Car Mini (Price 14000)
+urCar = Car Mazda (Price 20000)
+clownCar = Car Tata (Price 7000)
+myPlane = Plane Papu
+
