befeb43e4656e5a81590151576a6e33b561a70cb
John Werner <varreli@gmail.com>
2018-09-08 18:47:17 -0700

    Synch

--- data/notesMonoid.hs
+++ data/notesMonoid.hs
@@ -12,3 +12,9 @@
 -- A "monoid" as we use them is a monoid object in some monoidal category 
 -- C where we choose cartesian products as the tensor. So a Haskell monoid 
 -- would be (Type, (,), ()) within Hask (which is cartesian monoidal).
+
+ -- A group is a monoid in which every element has an inverse. So the integers 
+-- Z is a group (under addition):  3  has the inverse -3, since  3 + (-3) = 0.  
+-- But the set {0,1,2,...} is only a monoid: it has the identity element 0, 
+-- and the operation is associative, but 3 has no inverse in this set. Nor 
+-- does anything else besides 0.
