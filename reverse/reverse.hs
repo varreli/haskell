828bd4252a22a28cd05564a906162edb47778cfe
varreli <varreli@gmail.com>
2018-09-15 21:31:21 -0700

    Synch

--- reverse/reverse.hs
+++
@@ -1,7 +0,0 @@
-module Reverse where
-rvrs :: String -> String
-
-rvrs x = concat [awesomeString, " ", isString, " ", curryString]
-  where curryString = take 5 x
-        isString = take 2 $ drop 6 x
-        awesomeString = take 7 $ drop 9 x
