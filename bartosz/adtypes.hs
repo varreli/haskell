42d7f5db704920c1c93db44a4d3bb5c62744fea8
varreli <varreli@gmail.com>
2018-09-18 22:31:38 -0700

    Add notes from youtube to adtypes.hs

--- bartosz/adtypes.hs
+++ bartosz/adtypes.hs
@@ -35,9 +35,31 @@ ffmap :: (a -> b) -> (ExprF a -> ExprF b)
 ffmap f (Const i) = Const i
 ffmap f (Add x y) = Add (f x) (f y)
 ffmap f (Mul x y) = Mul (f x) (f y)
-
+ 
 
 -- With Add or Mul :
 
--- Add (f x) (f y)      -- we are operating on two a types and
--- Mul (f x) (f y)      -- returning two b types.
+-- Add (f x) (f y)      -- we are operating on two a types 
+-- Mul (f x) (f y)      -- and returning two b types.
+
+data Maybe a = Nothing | Just a  
+
+-- The kind of Maybe is * -> * 
+-- It takes a type and returns a type.
+-- This is what Functor needs.
+
+-- map is a specific implementation of fmap to apply
+-- functions to lists only.
+-- fmap is the generalization of map. If we want to
+-- apply a function to another data structure, say a
+-- Tree, we use fmap.
+
+fmap :: (a -> b) -> f a -> f b
+
+-- fmap applies the function (a -> b) to everything in 
+-- the data structure with elements a called f a . It then
+-- returns that same data structure with the elements
+-- transformed. fmap must know how to traverse the data
+-- structure.
+
+-- https://www.youtube.com/watch?v=4tXna2Al3Ps
