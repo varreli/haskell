676d5440955393b7f90a906511ee15bff3ea7b29
varreli <varreli@gmail.com>
2018-09-24 15:37:39 -0700

    Add mappend.hs to bartosz

---
+++ bartosz/mappend.hs
@@ -0,0 +1,20 @@
+import Data.Monoid
+
+main = do
+    let a = [1,1,2]
+        b = [3,5]
+        c = [8,13]
+
+    putStrLn "These 3 are equal:"
+    print $  foldr (<>) mempty [a, b, c]
+    print $ a ++ (b ++ c)
+    print $ (a ++ b) ++ c
+    putStrLn "\nThese leave 'a' alone:"
+    print $ a ++ []
+    print $ [] ++ a
+
+-- --------------------------------------
+
+    putStrLn "\nThese are equal:"
+    print $ "Hello there!" <>  mempty <> " Monoids are"  <> " neat!" <> mempty
+    print $ "Hello there!" <> (mempty <> " Monoids are") <> " neat!" <> mempty
