b29e923a63791d8181cdfd505b9d18eef15c9d3d
John Werner <varreli@gmail.com>
2018-09-26 10:46:51 -0700

    Synch

--- bartosz/mappend.hs
+++ bartosz/mappend.hs
@@ -1,4 +1,5 @@
 import Data.Monoid
+import Safe (readMay)
 
 main = do
     let a = [1,1,2]
@@ -27,3 +28,13 @@ tt = (<>) (Just "Peace") (Just " Out")  -- (<>) is mappend
 
 -- > tt <> mempty <> Nothing <> Just " Dude"
 -- Just "Peace Out Dude"
+
+
+numbers :: Maybe [Int]
+numbers = readMay "[1,2,3]" <> readMay "[90" <> readMay "[4,5,6]" <> mempty
+
+vals :: [Maybe Int]
+vals = [readMay "ignored", readMay "1", readMay "5"]
+
+aa = print . mconcat $ map First vals
+bb = print . mconcat $ map Last  vals
