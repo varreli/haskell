d6fe55209ce628bdc962f053d2eeacb692019ac5
varreli <varreli@gmail.com>
2018-09-27 17:15:34 -0700

    Synch

--- bartosz/pb.hs
+++ bartosz/pb.hs
@@ -1,19 +1,18 @@
 import Data.Monoid
 import Safe (readMay)
 
-
--- main = print numbers
-
-numbers :: Maybe [Int]
-numbers = readMay "[1,2,3]" <> readMay "[90" <> readMay "[4,5,6]" <> mempty
-
 vals :: [Maybe Int]
 vals = [readMay "ignored", readMay "1", readMay "5"]
 
 values :: [Maybe String]
-values = [readMay "\"balloon\"", readMay "1", readMay "5"]
+values = [readMay "1", readMay "\"balloon\"", readMay 5]
 
 aa = print . mconcat $ map First vals
 bb = print . mconcat $ map Last  vals
 
 cc = print . mconcat $ map First values
+
+-- iceland_Jack: when we run cc , the String "balloon" is
+-- not read, but in aa and bb "1" and "5" 
+-- (which I would hope are String elements) are read.
+-- This is strange.
