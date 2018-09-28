b299eb6e7abeb5645dc527369a3415f1388c7b64
varreli <varreli@gmail.com>
2018-09-27 22:15:44 -0700

    Synch

--- bartosz/mappend2.hs
+++ bartosz/mappend2.hs
@@ -5,7 +5,7 @@ import Safe (readMay)
 -- main = print numbers
 
 numbers :: Maybe [Int]
-numbers = readMay "[1,2,3]" <> readMay "[90" <> readMay "[4,5,6]" <> mempty
+numbers = readMay "[1,2,3]" <> readMay "[90" <> readMay "[4,5,6]" <> mempty 
 
 vals :: [Maybe Int]
 vals = [readMay "ignored", readMay "1", readMay "5"]
