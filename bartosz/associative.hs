71b42edad95ce9995b997362fda1f2feb3f0435a
varreli <varreli@gmail.com>
2018-09-28 22:15:07 -0700

    Add associative to bartosz

---
+++ bartosz/associative.hs
@@ -0,0 +1,17 @@
+v1 :: String -> Int
+v1 = length . filter (not . null) . lines
+
+v2 :: String -> Int
+v2 = fg . lines
+  where
+        fg = length . filter (not . null)
+
+v3 :: String -> Int
+v3 = length . gh
+  where
+        gh = filter (not . null) . lines
+
+main = do
+    print $ v1 "a\n\nb\nc"
+    print $ v2 "a\n\nb\ncxxx"
+    print $ v3 "a\n\nb\nc"
