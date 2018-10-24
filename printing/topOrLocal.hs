ae6ce8bcbb34d9f9f5a6235028c39921704233a7
John Werner <varreli@gmail.com>
2018-10-23 18:14:19 -0700

    Synch

--- printing/topOrLocal.hs
+++
@@ -1,9 +0,0 @@
-module TopOrLocal where
-
-topLevelFunction :: Integer -> Integer
-topLevelFunction x = x + woot * topLevelValue
-  where woot :: Integer
-        woot = 10 -- local binding defined
-
-topLevelValue :: Integer
-topLevelValue = 5
