0e5e28402894391f1fd7f705f5b7394527783f88
varreli <varreli@gmail.com>
2018-10-31 15:56:02 -0700

    Add to math

--- not_book/prime-factors.hs
+++ not_book/prime-factors.hs
@@ -5,18 +5,23 @@ import Debug.Trace
 divides :: Integer -> Integer -> Bool
 divides d n = rem n d == 0
 
+
 ldf :: Integer -> Integer -> Integer
-ldf k n   | divides k n = k
+
+ldf k n     | divides k n = k
             | k^2 >= n = n
             | otherwise = ldf (k+1) n
 
+
 ld :: Integer -> Integer
+
 ld n = ldf 2 n
+
 
 factors :: Integer -> [Integer]
 factors n | n < 1 = error "argument not positive"
           | n == 1 = []
-          | otherwise = traceShowId p : factors (div n p) where p = ld n
+          | otherwise = {-- traceShowId --} p : factors (div n p) where p = ld n
 
 main = print (factors 99) >> print (factors 5390)
 
