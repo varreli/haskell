8373b4a263aa61d7fe6b35aeeeb400730b19bfd3
varreli <varreli@gmail.com>
2018-11-01 16:54:23 -0700

    Synch

--- bartosz/functor.hs
+++ bartosz/functor.hs
@@ -1,10 +1,9 @@
-λ > tx = (<$>) length
-
-λ > :t tx
 tx :: (Functor f, Foldable t) => f (t a) -> f Int
+tx = (<$>) length
 
-λ > tx [(Just 4.5), (Just 2), (Just 3.3), Nothing, (Just 5.66)]
-[1,1,1,0,1]
+
+-- λ > tx [(Just 4.5), (Just 2), (Just 3.3), Nothing, (Just 5.66)]
+-- [1,1,1,0,1]
 
 -- This returns a function operating on a functor (given 
 -- constraints on the element type originating from 
