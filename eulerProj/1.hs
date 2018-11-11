3f72118fae4e6bf65d3817f00c625e6fe09e21c4
John Werner <varreli@gmail.com>
2018-11-10 19:30:56 -0800

    Synch

--- eulerProj/1.hs
+++ eulerProj/1.hs
@@ -1,9 +1,12 @@
 import Data.List (union)
 
-euler1 = sum (union [3,6..999] [5,10..999])
+euler1 = union [3,6..19] [5,10..19]
 
-main = print $ prob_1
+euler2 = union [2,4..28] [7,14..56]
 
+main = do
+	print $ euler1
+        print $ euler2
 
 -- The list of all natural numbers below 10 that are multiples 
 -- of 3 or 5 are 3, 5, 6 and 9. The sum of these multiples is 23.
--- eulerProj/2.hs
+++ eulerProj/2.hs
@@ -1,5 +1,12 @@
 -- A lazy infinite sequence of fib numbers:
 
-fib = (let x = 1 : 2 : zipWith (+) x (tail x) in x)
 
-sumFibs = sum $ filter even $ takeWhile (<= 4000000) fib
+
+fib = (let x = 1 : 1 : zipWith (+) x (tail x) in x)
+
+sumFibs = sum $ filter even $ takeWhile (<= 400) fib
+
+
+productFibs = foldr (*) 1 $ takeWhile (<= 10) fib 
+
+-- note seed is 1 for multiplication
