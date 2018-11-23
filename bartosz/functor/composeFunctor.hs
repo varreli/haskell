10a3147a45869a1d6d200f515729da7b8a369c51
varreli <varreli@gmail.com>
2018-11-22 19:39:07 -0800

    Synch

--- bartosz/functor/composeFunctor.hs
+++ bartosz/functor/composeFunctor.hs
@@ -1,11 +1,14 @@
--- instance Functor ((->) r) where
+-- instance Functor ((->) r) where       -- (->) is partially applied
 --    fmap = (.)
 
 -- fmap :: (b -> c) -> (a -> b) -> (a -> c) 
 -- fmap :: (a -> b) -> (r -> a) -> (r -> b)
 
+composer :: (a -> b) -> (r -> a) -> (r -> b)
+composer f g = f . g 
+
 composerr :: (a -> b) -> (r -> a) -> (r -> b)
-composerr f g x = f (g x) 
+composerr = (.)
 
 composerr' :: (a -> b) -> (r -> a) -> (r -> b)
-composerr' = (.) 
+composerr' = fmap 
