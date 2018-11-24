6c85d9acee92c21dab4a3235cfea5c555573d5cf
varreli <varreli@gmail.com>
2018-11-23 21:46:11 -0800

    Add monomorph show parameter.

--- bartosz/functor/functorLen.hs
+++ bartosz/functor/functorLen.hs
@@ -9,13 +9,6 @@ b = (<$>) length
 -- constraints on the element type originating from 
 -- length, namely Foldable)
 
--- λ > tx [(Just 4.5), (Just 2), (Just 3.3), Nothing, (Just 5.66)]
--- [1,1,1,0,1]
-
--- This returns a function operating on a functor (given 
--- constraints on the element type originating from 
--- length, namely Foldable)
-
 -- Functor wants a type constructor that takes one type 
 -- and not a concrete type:
 
@@ -24,10 +17,3 @@ b = (<$>) length
 -- Either a   -- b makes it concrete
 -- Tree
 
--- Functor wants a type constructor that takes one type 
--- and not a concrete type:
-
--- a : []
--- Maybe
--- Either a   -- b makes it concrete
--- Tree
---
+++ bartosz/functor/monomorph.hs
@@ -0,0 +1,27 @@
+aa :: [Maybe String] -> [Maybe String] 
+aa = (<$>) (fmap (++ " !")) 
+
+
+-- haskell assumes you intended a memoized value binding, 
+-- and memoization only makes sense with a concrete type, 
+-- so it monomorphizes. It needs a parameter on lhs :
+
+bb :: (Functor f1, Functor f) => t -> f (f1 [Char]) -> f (f1 [Char]) 
+
+bb x = (<$>) (fmap (++ " !"))   -- x must be show
+
+
+-- With no eta reduction its equivalent to aa above :
+
+cc :: [Maybe String] -> [Maybe String] 
+cc x = (<$>) (fmap (++ " !")) x
+
+
+-- With a concrete type (note dd signature) :
+
+dd :: [Maybe String]
+dd = (<$>) (fmap (++ "! ")) [(Just "Yo"), Nothing, (Just "Hey")]
+
+
+-- In category theory, morphisms are a generalization of functions.
+-- Monomorphisms correspond specifically to injective functions.
