a5733296693d63d79e66a88e157aa9d5df0d927f
varreli <varreli@gmail.com>
2018-11-25 21:57:38 -0800

    Synch

--- bartosz/functor/counter.hs
+++ bartosz/functor/counter.hs
@@ -7,3 +7,7 @@ instance Functor CMaybe where
     fmap f CNothing = CNothing
     fmap f (CJust counter x) = CJust (counter +1) (f x)
 
+
+t = (<$>) (map toUpper) (fmap (intersperse '-') (CJust 0 "hoot"))
+
+-- note use of (map toUpper) in line 11
