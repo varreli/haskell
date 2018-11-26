9926269cef191380d877ae621179aac0e03ae04a
John Werner <varreli@gmail.com>
2018-11-25 18:53:46 -0800

    Add test functor

---
+++ bartosz/functor/test.hs
@@ -0,0 +1,9 @@
+import Data.List
+import Data.Char
+
+data CMaybe a = CNothing | CJust Int a deriving Show
+
+instance Functor CMaybe where
+    fmap f CNothing = CNothing
+    fmap f (CJust counter x) = CJust (counter +1) (f x)
+
