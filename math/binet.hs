1c6cde9ac019a3504d8d81cfa12f5225e572efde
John Werner <varreli@gmail.com>
2019-01-12 18:08:51 -0800

    Add notes for :set XExtension

--- math/binet.hs
+++ math/binet.hs
@@ -1,18 +1,18 @@
 -- 2 versions of binet's formula to find nth fibonacci:
 
-fib n = round $ phi ** fromIntegral n / sq5
+fib n  = round $ phi ** fromIntegral n / sq5
   where
     sq5 = sqrt 5 :: Double
     phi = (1 + sq5) / 2
 
 
-fib' n = (binet - binet') / (sqrt 5)
+fib' n = round $ (binet - binet') / sqrt 5    
     where
       binet   = ((1 + sqrt 5) /2) ^ n 
       binet'  = ((1 - sqrt 5) /2) ^ n 
 
 
--- two standard haskell ways to find fibonaccis:
+-- 2 standard haskell ways to find fibonaccis:
 
 fibs = take 10 (let x = 1 : 1 : zipWith (+) x (tail x) in x)
 
