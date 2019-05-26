93946185002f55842529fad1b5fed61f312e485f
varreli <varreli@gmail.com>
2019-05-25 21:04:32 -0700

    https://math.stackexchange.com/questions/9259/find-the-sum-of-all-the-multiples-of-3-or-5-below-1000

--- math/eulerProj/1.hs
+++ math/eulerProj/1.hs
@@ -5,7 +5,7 @@ euler1 = union [3,6..19] [5,10..19]
 euler2 = union [2,4..28] [7,14..56]
 
 main = do
-	print $ euler1
+        print $ euler1
         print $ euler2
 
 -- The list of all natural numbers below 10 that are multiples 
@@ -13,3 +13,11 @@ main = do
 
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
+euler3 = [3,6..999]
+euler4 = [5,10..999]
+t = foldr (+) 0 [3,6..999]
+tt = foldr (+) 0 [5,10..999]
+-- sub = foldr (+) 0 [15..990]
+sub = union [3,6..999] [5,10..999] -- wrong
+
+answer = ((+) t tt) - sub
