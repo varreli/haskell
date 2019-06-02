3aa4c5144baf3e40f6b2f3091d39e3d4f91272e4
John Werner <varreli@gmail.com>
2019-06-01 17:49:26 -0700

    Synch

---
+++ math/f.hs
@@ -0,0 +1,21 @@
+-- euclids finds the GCD greatest common divisor 
+-- (greatest common factor)
+
+import Debug.Trace
+
+euclid :: Int -> Int -> Int
+euclid s t | traceShow (s, t) False = undefined
+euclid s t
+  | s <= 0, t <= 0 = 0
+  | s == t = s
+  | s < t = euclid s ( t - s )
+  | s > t = euclid ( s - t ) t
+
+--  λ > euclid 216 594
+-- (216,594)
+-- (216,378)
+-- (216,162)
+-- (54,162)
+-- (54,108)
+-- (54,54)
+-- 54
