aeee9a6cea07e00ad73e79d55f3e25280e15c830
John Werner <varreli@gmail.com>
2019-06-06 12:26:43 -0700

    Add terse totient func

--- math/totient.hs
+++ math/totient.hs
@@ -7,3 +7,14 @@ factor n = let divisors = dropWhile ((/= 0) . mod n) [2 .. ceiling $ sqrt $ from
 
 
 totient n = round $ (fromIntegral n) * (product $ map (\x -> 1 - 1/(fromIntegral x)) (factor n))
+
+
+-- factor :: Integer -> [Integer]
+-- 
+-- factor 1 = []
+-- factor n = let divisors = dropWhile ((/= 0) . mod n) [2 .. ceiling $ sqrt $ fromIntegral n]
+--            in let prime = if null divisors then n else head divisors
+--               in (prime :) $ factor $ div n prime
+-- 
+-- 
+-- totient n = round $ (fromIntegral n) * (product $ map (\x -> 1 - 1/(fromIntegral x)) (factor n))
---
+++ math/totient2.hs
@@ -0,0 +1,13 @@
+-- Calculate Euler's totient function phi(m).
+
+-- Euler's totient function phi(m) is defined
+-- as the number of positive integers 
+
+-- r (1 <= r < m) 
+
+--that are coprime to m.
+
+
+totient 1 = 1
+totient a = length $ filter (coprime a) [1..a-1]
+ where coprime a b = gcd a b == 1
