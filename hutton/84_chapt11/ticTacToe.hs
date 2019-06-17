8a4f7ff09022a19b4a542e167036c08e3304455f
varreli <varreli@gmail.com>
2019-06-17 14:35:24 -0700

    Begin tictactoe

--- hutton/84_chapt11/ticTacToe.hs
+++ hutton/84_chapt11/ticTacToe.hs
@@ -3,11 +3,27 @@ import Data.List
 import System.IO
 
 size :: Int
-size = 2
+size = 3 
 
 type Grid = [[Player]]
 
 data Player = O | B | X
               deriving (Eq, Ord, Show)
 
+next :: Player -> Player
+next O = X
+next B = B
+next X = O
 
+empty :: Grid
+empty = replicate size (replicate size B)
+
+full :: Grid -> Bool
+full = all (/= B) . concat
+
+whoseTurn :: Grid -> Player
+whoseTurn g = if os <= xs then O else X
+  where
+    os = length (filter (== O) ps)
+    xs = length (filter (== X) ps)
+    ps = concat g
