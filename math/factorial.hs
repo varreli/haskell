350939c8d373fbaff2b0acbcf2426d245a3e5cd0
varreli <varreli@gmail.com>
2019-06-18 16:26:57 -0700

    Minor factorial tweak

--- math/factorial.hs
+++ math/factorial.hs
@@ -5,7 +5,15 @@ fact n = product [1..n] -- less elegant: fact n = n * fact (n-1)
 main = print $ map fact [12, 4, 7]
 
 
+-- less elegant with much larger Integer range :
+
+fact' :: Integer -> Integer 
+fact' 0 = 1
+fact' n = n * fact' (n-1)
+
+
 -- https://en.wikipedia.org/wiki/Proof_assistant#Comparison
 -- https://en.wikipedia.org/wiki/Dependent_type#Comparison
 
 -- http://stackoverflow.com/questions/6806946/built-in-factorial-function-in-haskell
+
