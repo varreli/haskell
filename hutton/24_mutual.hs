b895870cd3af75ee6b07de9ec733a2fa1af90470
John Werner <varreli@gmail.com>
2019-06-24 18:28:50 -0700

    Synch

--- hutton/24_mutual.hs
+++ hutton/24_mutual.hs
@@ -19,3 +19,16 @@ evens (x:xs) = x : odds xs
 odds :: [a] -> [a]
 odds [] = []
 odds (_:xs) = evens xs  -- the head is dropped here
+
+
+-- evens 1 : odds [2,3,44]
+-- 
+-- 1 : odds [3,44]
+-- 
+-- 1 : 3 : odds [44]
+-- 
+-- 1 : 3 : odds [] 
+-- 
+-- odds [] = 1:3:[]
+-- 
+-- [1,3] 
