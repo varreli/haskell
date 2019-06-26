ea53b65216e4ee74cafb2a6c1e846f7547b17722
John Werner <varreli@gmail.com>
2019-06-25 19:51:36 -0700

    Worked on gg et al

---
+++ gg/gg4.hs
@@ -0,0 +1,44 @@
+
+> :t id
+a -> a
+
+-- specialize this for flip:
+
+(b -> c) -> (b -> c)
+(b -> c) -> b -> c
+
+-- then (b -> c) and b are flipped:
+
+> :t flip id
+b -> (b -> c) -> c  
+-- (b -> c) is unary since all functions in
+-- haskell are unary. but the ghc is polymorphic
+-- so this function can take n arguments.
+
+
+λ >  flip id :: b -> (b -> c) -> c
+λ > :t flip id 4 (/)
+flip id 4 (/) :: Fractional b => b -> b
+λ > :t flip id 4 (/) 2
+flip id 4 (/) 2 :: Fractional b => b
+
+
+-- y x is a function application: we're 
+-- applying `y` to `x` :
+
+-- flip id x f y => f x y
+
+-- flip id 4 (/) 2     is equivalent to :
+-- flip ($) 4 (/) 2
+
+
+-- id is gone after you apply flip to it:
+
+-- > flip id 4 (/) 3
+-- > flip 4 (/) 3
+-- > (/) 4 3
+-- 1.33333
+
+-- study : http://evan-tech.livejournal.com/240180.html
+
+-- study : https://github.com/uwedeportivo/NikePlus-Cocoa-RunLog/wiki/publishing-nike-runs,-intermission%3A-flip-id
