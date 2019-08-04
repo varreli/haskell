df0da51f403b4082d6667b5ae04df8f93889dce3
varreli <varreli@gmail.com>
2019-08-03 21:20:56 -0700

    Rename files

--- hutton/85_chapt12/appTopLevel.hs
+++ hutton/85_chapt12/appTopLevel.hs
@@ -2,5 +2,14 @@ data List a = Nil | Cons a (List a) deriving Show
 
 app :: List a -> List a -> List a 
 app Nil ys = ys
-app (Cons x xs) ys = Cons x (xs `app` ys)
+app (Cons x xs) ys = Cons x (app xs ys)
+
+-- > app (Cons 0 (Cons 5 Nil)) (Cons 10 (Cons 15 Nil)
+-- Cons 0 
+-- Cons 0 (Cons 5 (Cons 10 (Cons 15 Nil))) 
+
+-- ( After Cons 5, the base case is reached -- app Nil ys = ys )
+
+
+
 
