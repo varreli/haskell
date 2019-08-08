a876f46a24c74dd94e7f5d70627ffbf99a89aad3
varreli <varreli@gmail.com>
2019-08-07 22:10:33 -0700

    Add products , two types

--- hutton/85_chapt12/applicative.hs
+++ hutton/85_chapt12/applicative.hs
@@ -13,12 +13,22 @@ fmap2 g x y = pure g <*> x <*> y          --   and so on in hierarchy
 fmap3 :: Applicative f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
 fmap3 g x y z = pure g <*> x <*> y <*> z
 
+----------------------------------------------------------------
+prods :: [Int] -> [Int] -> [Int]
+prods xs ys = [x*y | x <- xs, y <- ys]   -- list comprehension
+                                         -- product
+
+
+products :: [Int] -> [Int] -> [Int]
+products xs ys = pure (*) <*> xs <*> ys  -- applicative product 
+----------------------------------------------------------------
+
 -- instance Applicative Maybe where
---  pure :: a -> Maybe a
---     pure = Just
---  (<*>) :: Maybe (a -> b) -> Maybe a -> Maybe b
---     Nothing <*> _ = Nothing
---     (Just g) <*> mx = fmap g mx
+-- --  pure :: a -> Maybe a
+--       pure = Just
+-- --  (<*>) :: Maybe (a -> b) -> Maybe a -> Maybe b
+--       Nothing <*> _ = Nothing
+--       (Just g) <*> mx = fmap g mx
 
 -- > fmapp (\x y z -> x * y + z) [10,100,1000] [3,4,5] [7,8,9]
 
