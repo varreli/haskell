b79729e138d8e7a09a6f2226cc2b700fbdfb797e
varreli <varreli@gmail.com>
2019-08-21 21:08:15 -0700

    Add notes to divZero.hs

--- hutton/85_chapt12/divByZero.hs
+++ hutton/85_chapt12/divByZero.hs
@@ -10,5 +10,19 @@ eval (Div x y) = case eval x of
                     Nothing -> Nothing
                     Just n -> case eval y of 
                                  Nothing -> Nothing
-                                 Just m -> safediv n m
+                                 Just m -> safediv n m 
 
+
+-- to convert to applicative style, this would fail 
+-- for two reasons:
+
+-- eval' :: Expr -> Maybe Int
+-- eval' (Val n) = pure n
+-- eval' (Div x y) = pure safediv <*> eval x <*> eval y
+
+-- 1) safediv has type Int -> Int -> Maybe Int, but this
+-- would need:         Int -> Int -> Int
+
+-- 2) replacing pure safediv would also fail, since the
+-- new function would need Maybe (Int -> Int -> Int) ,
+-- which has no provision for divide by zero.
