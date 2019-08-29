86aa9dfbcbe4b32501f227912422ba9b4c582c49
varreli <varreli@gmail.com>
2019-08-28 17:31:14 -0700

    Add notes to 85

---
+++ palindrome/halfPalin.hs
@@ -0,0 +1,9 @@
+main = do
+  putStrLn "Enter just over half of a palindrome:"
+  s <- getLine
+  putStrLn (s ++ tail (reverse s))
+
+
+-- gohangasalami.          (note '.')
+-- murderforajarofredrum   (murderforaj)
+
