58d2ca85a4a2af37bba184c9cfe94ad4ba959c6c
varreli <varreli@gmail.com>
2019-08-30 22:41:55 -0700

    Add State Monad

--- hutton/85_chapt12/state.hs
+++ hutton/85_chapt12/state.hs
@@ -24,13 +24,18 @@ instance Applicative ST where
 -- re: instance Applicative ST :
 
 -- The state being passed from `stf` to `stx` is s'
--- 
+ 
 -- giving state to stf results in a tuple of a new state 
 -- called s' and a function called f. this is what the 
 -- line "let (f,s') = app stf s" is doing.
--- 
+ 
 -- that new state s' is then used to give state to stx, 
 -- giving another new state called s'' and a value 
 -- called x. the last thing we do is apply our function 
 -- f to our value x because that's what our type signature 
 -- requires, and bundle that with the last state.
+
+instance Monad ST where
+-- (>>=) :: ST a -> (a -> ST b) -> ST b
+  st >>= f = S (\s -> let (x,s') = app st s
+                          in app (f x) s')
