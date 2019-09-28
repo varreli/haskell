8f2ce29f82c6ae9ffd78ac75158d2079915e4fcf
varreli <varreli@gmail.com>
2019-09-27 17:19:27 -0700

    Add study notes curryUncurry

--- bartosz/isomorphism/curryUncurry.hs
+++ bartosz/isomorphism/curryUncurry.hs
@@ -3,3 +3,12 @@ curryy f x y = f (x, y)
 
 uncurryy :: (a -> b -> c) -> (a, b) -> c
 uncurryy f p = f (fst p) (snd p)
+
+cury f = g
+    where g x y = f (x,y)
+
+uncury f (s,t) = f s t
+
+
+-- curry converts functions that normally take tuple arguments f (a,b).
+-- uncurry converts functions that normally take parameter arguments f a b.
