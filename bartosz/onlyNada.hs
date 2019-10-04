b1c8b4c468521a7cf128dc2505d5f960f3a32124
varreli <varreli@gmail.com>
2019-10-03 22:42:00 -0700

    Add onlyNada to monoid files in Bartosz

---
+++ bartosz/onlyNada.hs
@@ -0,0 +1,16 @@
+import Data.Monoid
+
+data Optional a = Nada | Only a deriving (Eq, Show)
+
+instance Monoid a => Monoid (Optional a) where
+      mempty = Nada
+      mappend Nada Nada = Nada
+      mappend (Only x) Nada = Only x
+      mappend Nada (Only x) = Only x
+      mappend (Only x) (Only y) = Only (x <> y)
+
+
+ss = print $ Nada `mappend` Only (Sum 8)
+tt = print $ mappend (Only (Product 5)) (Only (Product 7))
+
+
