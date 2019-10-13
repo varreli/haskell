e39059b426880cbbd751becab23dd7fa9a0e7f20
John Werner <varreli@gmail.com>
2019-10-12 17:41:59 -0700

    Synch

--- hutton/85_chapt12/applicative.hs
+++ hutton/85_chapt12/applicative.hs
@@ -4,15 +4,15 @@
 
 fmap0 :: Applicative f => a -> f a   -- pure has "return-type polymorphism"
 fmap0 = pure                         -- so the type of the return type
-				     -- dictates which instance (implementation) 
-				     -- of pure to use
+                                     -- dictates which instance (implementation) 
+                                     -- of pure to use
 
 
 fmap1 :: Applicative f => (a -> b) -> f a -> f b
 fmap1 g x = pure g <*> x                             -- standard case
 
 fmap2 :: Applicative f =>  (a -> b -> c) -> f a -> f b -> f c   
-fmap2 g x y = pure g <*> x <*> y          --   and so on in hierarchy
+fmap2 g x y = pure g <*> x <*> y                     -- and so on in hierarchy
 
 fmap3 :: Applicative f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
 fmap3 g x y z = pure g <*> x <*> y <*> z
@@ -31,7 +31,7 @@ products xs ys = pure (*) <*> xs <*> ys  -- applicative product
 -- [38,39,58,59,308,309,508,509]
 
 -- > fmap3 (\x y z -> x * y ^ z) (Just 10) (Just 3) (Just 7)
--- Just 135
+-- Just 21870 
 
 ----------------------------------------------------------------------
 
