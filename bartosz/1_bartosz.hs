b82ff6e1c7685fed8374c72c3c421bd71ac79645
John Werner <varreli@gmail.com>
2019-10-18 21:02:38 -0700

    Edit irc_notes

--- bartosz/1_bartosz.hs
+++ bartosz/1_bartosz.hs
@@ -1,6 +1,5 @@
 import Data.Monoid
-import Safe (readMay)  --  $ cabal install safe    first
-
+import Safe (readMay)     --   $ cabal install safe     first
 main = do
     let a = [1,1,2]
         b = [3,5]
@@ -25,16 +24,39 @@ ff = flip (++)
 tt = (<>) (Just "Peace") (Just " Out")  -- (<>) is mappend
 
 -- note:
-
 -- > tt <> mempty <> Nothing <> Just " Dude"
 -- Just "Peace Out Dude"
 
-
 numbers :: Maybe [Int]
-numbers = readMay "[1,2,3]" <> readMay "[90]" <> readMay "[4,5,6]" <> mempty
+numbers = readMay "[1,2,3]" <> readMay "[90" <> readMay "[4,5,6]" <> mempty 
+-- ( "[90" will fail )
 
 vals :: [Maybe Int]
 vals = [readMay "ignored", readMay "1", readMay "5"]
 
 aa = print . mconcat $ map First vals
 bb = print . mconcat $ map Last  vals
+
+--
+
+values :: [Maybe String]
+values = [readMay "\"balloon\"", readMay "1", readMay "5"]
+
+cc = print . mconcat $ map First values
+
+--
+
+valls :: [Int]
+valls = [3,7,11]
+
+pp = print . mconcat $ map Sum valls
+qq = print . mconcat $ map Product valls
+
+--
+
+vallues :: [Bool]
+vallues = [False,False,True]
+
+rr = print . mconcat $ map All vallues
+ss = print . mconcat $ map Any vallues
+
