de81c6da7573de612018fe35e3b1dea73ace19bb
John Werner <varreli@gmail.com>
2019-10-20 21:52:34 -0700

    Add eitherFunc

---
+++ bartosz/functor/eitherFunc.hs
@@ -0,0 +1,27 @@
+-- Logic of (Either a b) being a functor:
+
+-- instance Functor (Either a) where
+--     fmap f (Right x) = Right (f x)
+--     fmap f (Left x) = Left x
+
+-- fmap :: (c -> d) -> f c -> f d
+
+-- Since (Either a) is the functor, we substitute it for f
+
+-- fmap :: (c -> d) -> Either a c -> Either a di
+
+----------------------------------------------------------------------
+
+-- The type of Right (f x) is
+-- Either a (f x)
+
+-- so (f x) is d :  Either a d     (the return type)
+
+-- The type of Left (f x) is
+
+-- Either (f x) b : Either d b     (not the return type, d must be in
+--                                 second parameter:  -> Either a d )               
+-- So we cannot fmap over Left 
+
+tt = fmap (*100) (Left 5)
+uu = fmap (*200) (Right 3)
