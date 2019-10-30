da869ce4f653fffa35d66addc4c364f437a3bc2e
John Werner <varreli@gmail.com>
2019-10-29 19:49:02 -0700

    Add appFunctor file

---
+++ bartosz/appFunctor/1_applicative.hs
@@ -0,0 +1,27 @@
+aa = fmap (++) (Just "Hey")
+bb = (\f -> f (" Joe"))
+jimi = bb <$> aa
+ 
+--------------------------------------- 
+cc = (*) <$> [2,5,10] <*> [8,10,11] 
+ 
+-- compare to list comprehension:
+ 
+dd xs ys = [x * y | x <- xs, y <- ys]
+---------------------------------------
+
+actions :: IO String  
+actions = (++) <$> getLine <*> getLine  
+
+action = do  
+     a <- (++) <$> getLine <*> getLine  
+     putStrLn $ "The two lines concatenated turn out to be: " ++ a  
+
+
+-- function application:
+
+-- instance Applicative ((->) r) where  
+--    pure x = (\_ -> x)  
+--    f <*> g = \x -> f x (g x)
+
+
