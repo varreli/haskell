48ab2f512881b6f521283f0ae427b49561923b51
John Werner <varreli@gmail.com>
2019-11-26 19:27:11 -0800

    add letBind.txt

---
+++ state/state2.hs
@@ -0,0 +1,7 @@
+newtype Staat s a = Staat { runstate :: s -> (a,s) }
+
+instance Functor (Staat s) where
+-- fmap :: (a -> b) -> Staat s a -> Staat s b
+    fmap f (Staat sa) = Staat (\s0 -> let (a, s1) = sa s0 in (f a, s1))
+
+dd = (*10) (Staat (2, "witch"))
