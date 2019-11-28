5ca6fa782c63953aa868decdc05039f9a1a78c86
John Werner <varreli@gmail.com>
2019-11-28 15:56:59 -0800

    Synch

--- hutton/85_chapt12/applicativeTest.hs
+++ hutton/85_chapt12/applicativeTest.hs
@@ -1,7 +1,5 @@
-
 fmap3 :: Applicative f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
 fmap3 g x y z = pure g <*> x <*> y <*> z
+main :: IO ()
+main = return ()
 
-main :: IO [()]
-
-main = fmap3 (\x y z -> x + y ^ z) [3,4] [5,6] [7,8]
