fc64f01736e78a2ffa4921c9a87ec0566793c0c4
varreli <varreli@gmail.com>
2019-11-28 17:29:36 -0800

    Synch

--- hutton/85_chapt12/applicativeTest.hs
+++ hutton/85_chapt12/applicativeTest.hs
@@ -1,5 +1,7 @@
 fmap3 :: Applicative f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
 fmap3 g x y z = pure g <*> x <*> y <*> z
-main :: IO ()
-main = return ()
 
+main :: IO [Integer]
+main = return $ fmap3 (\x y z -> x + y ^ z) [1,2] [2,3] [2,3]
+
+ 
