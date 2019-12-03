83600636940a91df8b2ce2cb6522c1afe8f08d5a
varreli <varreli@gmail.com>
2019-12-02 21:59:42 -0800

    Synch

--- hutton/85_chapt12/doNotation.hs
+++ hutton/85_chapt12/doNotation.hs
@@ -2,7 +2,6 @@
 
 kk = [(x,y) | x <- [1,2,3], y <- [1,2,3], (x /= y)]
 
-----------------------------------------------------
 
 ll = do x <- [1,2,3]
         y <- [1,2,3]
@@ -10,28 +9,36 @@ ll = do x <- [1,2,3]
         return (x,y)
 
 
-
 mm = do x <- [1,2,3]
         y <- [1,2,3]
         True <- ([x /= y] :: [Bool])
         return (x,y)
 
-----------------------------------------------------
+-- Q: How do we chart the evaluation steps of nn ? :
+
+
 
 nn = [1,2,3] >>= (\x -> [1,2,3] >>= (\y -> return (x /= y) >>= 
         (\r -> case r of True -> return (x,y)
                          _    -> fail "")))
 
--- (Substituting <&> or (flip fmap) for >>=   
--- has so far failed)
 
-----------------------------------------------------
+-- A: An irc member made the following suggestions, in
+--    this order:
+
+-- 1. Binding happens at the top level, so it just 
+--    looks like the end result.
+
+-- 2. We can substitute <&> or (flip fmap) for >>=   
+
+-- 3. We can employ oo below to chart evaluation steps:
+
 oo = concatMap (\x -> [x,x]) [1,2,3]
 
+-- 4. For lists,  xs >>= f = concatMap f xs ; bind is 
+-- literally concatMap.
 
--- For lists,  xs >>= f = concatMap f xs  :
--- bind is literally concatMap.
--- replace bind with fmap (concatMap with map) to see the 
+-- 5. Replace bind with fmap (concatMap with map) to see the 
 -- tree that eventually gets collapsed.
 -- We get a large nested list of lists 
 
@@ -39,10 +46,4 @@ oo = concatMap (\x -> [x,x]) [1,2,3]
 
 -- and their full concatenation is the final result
 
-----------------------------------------------------
 
--- Q: Considering evaluation steps, what would [1,2,3] 
--- look like after it was bound to (\x -> [1,2,3] ...) ?
-
--- A: Binding happens at the top level, so it just 
--- looks like the end result.
