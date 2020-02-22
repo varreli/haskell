4fe704730ff581c6b682fb0a52d9c5f80184dddc
John Werner <varreli@gmail.com>
2020-02-21 16:00:56 -0800

    Add kleisli.hs to monads file

---
+++ monads/kleisli.hs
@@ -0,0 +1,11 @@
+import Data.Char
+
+-- note average is (a -> b)
+average :: [Double] -> Double
+
+average n = sum n / fromIntegral (length n)
+
+(|>) :: (a -> b) -> (b -> c) -> (a -> c)
+(|>) = flip (.)
+
+vv = map toLower |> map toUpper
