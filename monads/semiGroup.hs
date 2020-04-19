48bc62812bfff8c40a46e45bddfa5f7641ce8a9c
John Werner <varreli@gmail.com>
2020-04-19 13:56:07 -0700

    Add json

--- monads/semiGroup.hs
+++ monads/semiGroup.hs
@@ -1,4 +1,17 @@
 
+
+newtype First a = First a deriving Show
+
+instance Semigroup (First a) where
+   a <> _ = a
+
+-----------------------------------------
+
+newtype Min a = Min a deriving Show
+
+instance Ord a => Semigroup (Min a) where
+   Min a <> Min b = Min (min a b)
+
 newtype First a = First a deriving Show
 
 instance Semigroup (First a) where
