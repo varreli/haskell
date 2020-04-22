b176a76a42bb10a2824b76f57654244c69c32b5a
John Werner <varreli@gmail.com>
2020-04-21 21:27:42 -0700

    Add mayybe.hs to monads

---
+++ monads/mayybe.hs
@@ -0,0 +1,16 @@
+ f :: Int -> Maybe Int
+ f 0 = Nothing
+ f x = Just (x-1)
+
+ g :: Int -> Maybe Int
+ g 100 = Nothing
+ g x = Just (x*10)
+
+ h :: Int -> Maybe Int
+ h x = case f x of
+         Just n -> g n
+         Nothing -> Nothing
+ 
+ h' :: Int -> Maybe Int
+ h' x = do n <- f x
+           g n
