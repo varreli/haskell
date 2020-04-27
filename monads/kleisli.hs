cb70d992e7001e5ca77a08703949aa719593cbf0
John Werner <varreli@gmail.com>
2020-04-27 14:14:49 -0700

    Synch

--- monads/kleisli.hs
+++ monads/kleisli.hs
@@ -1,27 +1,22 @@
--- {-# LANGUAGE FlexibleContexts #-}
-
 import Data.Char
 import Control.Monad
+
+
+(|>) :: (a -> b) -> (b -> c) -> (a -> c)
+(|>) = flip (.)
+
+
+(!>) :: (a -> m b) -> (b -> m c) -> (a -> m c)
+(!>) f g = f |> fmap g |> join 
+
+
+
+vv = map toLower |> map toUpper -- |> map toLower
 
 -- note average is (a -> b)
 average :: [Double] -> Double
 
 average n = sum n / fromIntegral (length n)
-
-(|>) :: (a -> b) -> (b -> c) -> (a -> c)
-(|>) = flip (.)
-
-vv = map toLower |> map toUpper -- |> map toLower
-
-
-ff = ?
-gg = ? 
-
-
-(!>) :: (Num a, Monad m) => (a -> m b) -> (b -> m c) -> (a -> m c)
-(!>) f g = f |> fmap g |> join 
-
-
 
 -- review now! :
 -- https://www.schoolofhaskell.com/user/Lkey/kleisli
