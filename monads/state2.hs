0201c50b5512c396a66ee1dccde3e15b697a58b0
John Werner <varreli@gmail.com>
2020-05-10 20:03:09 -0700

    Synch

--- monads/state2.hs
+++ monads/state2.hs
@@ -1,5 +1,7 @@
-newtype Staat s a = Staat { runstate :: s -> (a,s) }
+newtype Staat s a = Staat { runstate :: s -> (a,s) } 
+ 
 
 instance Functor (Staat s) where
 -- fmap :: (a -> b) -> Staat s a -> Staat s b
     fmap f (Staat sa) = Staat (\s0 -> let (a, s1) = sa s0 in (f a, s1))
+
