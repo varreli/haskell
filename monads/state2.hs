789f57d3c5c665eb0704856c6e5e4dd75ff39405
varreli <varreli@gmail.com>
2020-05-11 20:17:19 -0700

    Delete redundant file

--- monads/state2.hs
+++
@@ -1,7 +0,0 @@
-newtype Staat s a = Staat { runstate :: s -> (a,s) } 
- 
-
-instance Functor (Staat s) where
--- fmap :: (a -> b) -> Staat s a -> Staat s b
-    fmap f (Staat sa) = Staat (\s0 -> let (a, s1) = sa s0 in (f a, s1))
-
