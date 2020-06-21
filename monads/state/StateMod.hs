2aac23048d822db449286b5d607e20e782520189
John Werner <varreli@gmail.com>
2020-06-21 15:43:24 -0700

    Add evTurn alternate to state

--- monads/state/StateMod.hs
+++ monads/state/StateMod.hs
@@ -16,15 +16,21 @@ instance Functor ST where
   fmap g st = S (\s -> let (x,s') = app st s
                            in (g x, s'))
 
-----------------------------------------------------------------
 
 instance Applicative ST where
  -- pure :: a -> ST a
     pure x = S (\s -> (x,s))
  -- (<*>) :: ST (a -> b) -> ST a -> ST b
-    stf <*> stx = S (\s -> let (f,s') = app stf s 
-                               (x,s'') = app stx s' 
-                               in (f x, s''))
+    (S fx) <*> (S gx) = S $ \s ->                -- this from evTurn
+      let (f, s')  = fx s
+          (x, s'') = gx s'
+       in (f x, s'')
+
+--     stf <*> stx = S (\s -> let (f,s') = app stf s       -- Hutton
+--                                (x,s'') = app stx s' 
+--                                in (f x, s''))
+
+-----------------------------------------------------------------------
 
 -- with Applicatives, an action can't depend on a 
 -- value from a previous action; in monad it can.
