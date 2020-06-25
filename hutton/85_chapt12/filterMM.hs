4f5c0b1a89c234d4da510585fba1c281b32da679
John Werner <varreli@gmail.com>
2020-06-25 15:46:42 -0700

    Read blog on filterMM !

--- hutton/85_chapt12/filterMM.hs
+++ hutton/85_chapt12/filterMM.hs
@@ -4,9 +4,16 @@ import Data.Char
 filterMM :: Monad m => (a -> m Bool) -> [a] -> m [a]
 filterMM p [] = return []
 filterMM p (x:xs) = do 
-                    tru <- p x
+                    b <- p x
                     ys <- filterMM p xs
-                    return (if tru then x:ys else ys)
+                    return (if b then x:ys else ys)
 
 powerset = (\x -> [True,False])
 -- ghci> filterMM powerset [2,3]
+-- [[2,3],[2],[3],[]]
+
+
+
+-- READ:
+
+-- https://blog.ssanj.net/posts/2018-04-10-how-does-filterm-work-in-haskell.html
