a21c52b586c05ff20d9e1056aee69c5afaae847e
varreli <varreli@gmail.com>
2020-06-25 00:02:19 -0700

    Synch with tru in filterMM

--- hutton/85_chapt12/filterMM.hs
+++ hutton/85_chapt12/filterMM.hs
@@ -2,11 +2,11 @@ import Control.Monad
 import Data.Char
 
 filterMM :: Monad m => (a -> m Bool) -> [a] -> m [a]
-filterMM f [] = return []
-filterMM f (x:xs) = do 
-                    b <- f x
-                    ys <- filterMM f xs
-                    return (if b then x:ys else ys)
+filterMM p [] = return []
+filterMM p (x:xs) = do 
+                    tru <- p x
+                    ys <- filterMM p xs
+                    return (if tru then x:ys else ys)
 
 powerset = (\x -> [True,False])
--- ghci> filterMM powerset [2,3,4]
+-- ghci> filterMM powerset [2,3]
