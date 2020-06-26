fa1a743cb2465e5aa61761acebc9e7c686fce749
John Werner <varreli@gmail.com>
2020-06-26 16:41:54 -0700

    Eval of filtMM powerset [4,5]

--- hutton/85_chapt12/filterMM.hs
+++ hutton/85_chapt12/filterMM.hs
@@ -1,4 +1,7 @@
+import Control.Applicative
 import Control.Monad
+
+powerset = (\x -> [True,False])           -- powerset function
 
 filterMM :: Monad m => (a -> m Bool) -> [a] -> m [a]
 filterMM p [] = return []
@@ -7,12 +10,61 @@ filterMM p (x:xs) = do
                     ys <- filterMM p xs
                     return (if b then x:ys else ys)
 
-powerset = (\x -> [True,False])
--- ghci> filterMM powerset [2,3]
--- [[2,3],[2],[3],[]]
 
 
 
--- READ:
+filtM p = foldr (\x -> liftA2 (\flg -> if flg then (x:) else id) (p x)) (pure [])
 
--- https://blog.ssanj.net/posts/2018-04-10-how-does-filterm-work-in-haskell.html
+-- Expanding out the point-free version (uses values acc, accx) :
+
+filtMM p = foldr (\x acc -> liftA2 (\flg1  accx -> if flg1 then (x:accx) else accx) (p x) acc) (pure [])
+
+
+-- applicative version with foldr, on list [4,5] :
+
+------------------------
+
+-- For the value of 5:
+
+-- x = 5
+-- acc = [[]]
+-- flags = [True, False]
+
+------------------------
+-- accx []
+-- flg1 True
+-- result = 5:[] => [5]
+------------------------
+-- accx []
+-- flg1 False
+-- result => []
+
+-- newacc = [[5], []]
+
+------------------------
+
+-- For the value of 4:
+
+-- x = 4
+-- acc = [[5], []]
+-- flags = [True, False]
+
+------------------------
+-- accx [5]
+-- flg1 True
+-- result = 4:[5] => [4,5]
+------------------------
+-- accx []
+-- flg1 True
+-- result = 4:[] => [4]
+------------------------
+-- accx [5]
+-- flg1 False
+-- result => [5]
+------------------------
+-- accx []
+-- flg1 False
+-- result => []
+-------------------------------
+
+-- newacc = [[4,5],[4],[5], []]
