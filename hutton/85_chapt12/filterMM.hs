05b6bb7332dd7efe2cd4a6fdc1004114594fea0d
John Werner <varreli@gmail.com>
2020-06-29 18:45:46 -0700

    Synch

--- hutton/85_chapt12/filterMM.hs
+++ hutton/85_chapt12/filterMM.hs
@@ -1,7 +1,7 @@
 import Control.Applicative
 import Control.Monad
 
-powerset = (\x -> [True,False])           -- powerset function
+powerset = (\x -> [True,False])
 
 filterMM :: Monad m => (a -> m Bool) -> [a] -> m [a]
 filterMM p [] = return []
@@ -13,7 +13,7 @@ filterMM p (x:xs) = do
 
 
 
-filtM p = foldr (\x -> liftA2 (\flg -> if flg then (x:) else id) (p x)) (pure [])
+filtM p = scanr (\x -> liftA2 (\flg -> if flg then (x:) else id) (p x)) (pure [])
 
 -- Expanding out the point-free version (uses values acc, accx) :
 
