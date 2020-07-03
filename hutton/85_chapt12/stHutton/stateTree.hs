8e614260c53eb7d2c3b37562e4716cc24e40bc72
varreli <varreli@gmail.com>
2020-07-02 21:25:07 -0700

    Add Cale State notes

--- hutton/85_chapt12/stHutton/stateTree.hs
+++ hutton/85_chapt12/stHutton/stateTree.hs
@@ -17,15 +17,19 @@ aLabel :: Tree a -> ST (Tree Int)
 aLabel (Leaf _) = Leaf <$> fresh
 aLabel (Node l r) = Node <$> aLabel l <*> aLabel r
 
+mlabel :: Tree a -> ST (Tree Int)
+mlabel (Leaf _)    = do n <- fresh
+                        return (Leaf n)
+ 
+mlabel (Node l r)  = do l' <- mlabel l
+                        r' <- mlabel r
+                        return (Node l' r')
 
 tree  :: Tree Char
 tree = Node (Node (Leaf 'a') (Leaf 'b')) (Leaf 'c')
 
-tree2 :: Tree Char 
-tree2 = Node (Node (Leaf 'a') (Leaf 'b')) (Leaf 'c')
-
 -- examples:
 
-rlabel = reLabel tree 4
-labelA = app (aLabel tree2) 4    -- Show error without app.
-
+reelabel = reLabel tree 4
+labelApp = app (aLabel tree) 4    -- Show error without app.
+monadLab = app (mlabel tree) 4
