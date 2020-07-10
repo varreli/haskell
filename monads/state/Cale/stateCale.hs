1e1cc66f91eebcf2a1ceaf2dadb54cd3d4f8d963
varreli <varreli@gmail.com>
2020-07-09 23:38:17 -0700

    Finish caleState.hs with minimal instance declarations

---
+++ monads/state/Cale/stateCale.hs
@@ -0,0 +1,36 @@
+import Control.Applicative      
+import Control.Monad (liftM, ap)
+
+
+instance Functor     (State s) where    -- |
+    fmap = liftM                        -- |  minimal instance
+                                        -- |  declarations
+instance Applicative (State s) where    -- |
+    pure  = return
+    (<*>) = ap
+
+
+
+newtype State s a = S { runState :: s -> (s, a) }
+
+-- runState :: State s a -> s -> (s,a)
+-- runState (S f) s = f s
+
+get :: State s s
+get = S (\s -> (s, s))
+
+put :: s -> State s ()
+put val = S (\s -> (val, ()))
+
+give :: a -> State s a
+give g = S (\s -> (s, g))
+
+
+
+instance Monad (State s) where
+
+-- (>>=) :: State s a -> (a -> State s b) -> State s b
+
+  x >>= f = S (\s -> let (s', v)  = runState x s 
+                         (s'', w) = runState (f v) s' 
+                         in (s'', w))
--- monads/state/StateMod.hs
+++ monads/state/StateMod.hs
@@ -1,7 +1,6 @@
 module StateMod where
 import Control.Monad
 
--- Many things in haskell are just complicated ways to talk about state:
 
 type State = Int
 newtype ST a = S (State -> (a, State)) 
@@ -27,40 +26,12 @@ instance Applicative ST where
           (x, s'') = gx s'
        in (f x, s'')
 
---     stf <*> stx = S (\s -> let (f,s') = app stf s       -- Hutton
---                                (x,s'') = app stx s' 
---                                in (f x, s''))
-
------------------------------------------------------------------------
-
--- with Applicatives, an action can't depend on a 
--- value from a previous action; in monad it can.
 
 
--- re: instance Applicative ST :
+--  stf <*> stx = S (\s -> let (f,s') = app stf s          -- Hutton
+--                                  (x,s'') = app stx s' 
+--                                  in (f x, s''))
 
--- The state being passed from `stf` to `stx` is s'
- 
--- giving state to stf results in a tuple of a new state 
--- called s' and a function called f. this is what the 
--- line        let (f,s') = app stf s        is doing.
- 
--- that new state s' is then used to give state to stx, 
--- giving another new state called s'' and a value 
--- called x. the last thing we do is apply our function 
--- f to our value x because that's what our type signature 
--- requires, and bundle that with the last state.
-
----------------------------------------------------------------
-
-{-- In this manner the bind operator for the state monad 
-integrates the sequencing of state transformers with the 
-processing of their result values. Note that within the 
-definition for >>= we produce a new state transformer f x , 
-whose behaviour may depend on the result value of the first 
-argument x, whereas with <*> we are restricted to using state 
-transformers that are explicitly supplied as arguments. As 
-such, using the >>= operator provides extra flexibility. --} 
 
 instance Monad ST where
 -- (>>=) :: ST a -> (a -> ST b) -> ST b
