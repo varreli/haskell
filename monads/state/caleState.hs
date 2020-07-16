4cd8bca2aeabd430336afaadbff68c38682924b0
John Werner <varreli@gmail.com>
2020-07-16 16:01:20 -0700

    Add BACK caleState.hs, used history in github

---
+++ monads/state/caleState.hs
@@ -0,0 +1,42 @@
+import Control.Applicative
+import Control.Monad (liftM, ap)
+
+
+instance Functor     (State s) where    -- |
+    fmap = liftM                        -- |  minimal instance
+                                        -- |  declarations
+instance Applicative (State s) where    -- |
+    pure  = return
+    (<*>) = ap
+
+--------------------------------------------------------------
+
+newtype State s a = S { runState :: s -> (s,a) }
+
+get :: State a a
+get = S (\n -> (n,n))
+
+put :: s -> State s ()
+put val = S (\s -> ( val, () ))
+
+give :: a -> State s a
+give g = S (\s -> (s,g))
+
+
+instance Monad (State s) where
+
+-- (>>=) :: State s a -> (a -> State s b) -> State s b
+
+
+  x >>= f = S (\s -> let (s', v)  = runState x s 
+                         (s'', w) = runState (f v) s' 
+                         in (s'', w))
+
+
+data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
+
+tree  :: Tree Integer 
+tree = Node (Node (Leaf 1) (Leaf 2)) (Leaf 3)
+
+gett = get  >>= (\n -> put (n+1))
+
