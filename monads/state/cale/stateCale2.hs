480577d75dac1947e5089288ec50e5f462158fb6
John Werner <varreli@gmail.com>
2020-07-23 21:29:54 -0700

    Add notes to stateCale2.hs, add full module

--- monads/state/cale/stateCale2.hs
+++ monads/state/cale/stateCale2.hs
@@ -1,40 +1,18 @@
-import Control.Applicative      
-import Control.Monad (liftM, ap) 
-
-instance Functor     (State s) where    -- |
-    fmap = liftM                        -- |  minimal instance
-                                        -- |  declarations
-instance Applicative (State s) where    -- |
-    pure  = return
-    (<*>) = ap
----------------------------------------------------------------
-
-newtype State s a = S { runState :: s -> (s, a) } 
-
--- runState :: State s a -> s -> (s,a)
--- runState (S f) s = f s
-
-get :: State s s
-get = S (\s -> (s, s))
-
-put :: s -> State s ()
-put val = S (\s -> (val, ()) )
+import StateCaleMod2 
 
 
-instance Monad (State s) where
+increment = get >>= \n -> put (n+1)   
+                                     
+pure' = get >>= \n -> return (n+5) 
+                                   
 
--- (>>=) :: State s a -> (a -> State s b) -> State s b
-  x >>= f = S (\s -> let (s', v) = runState x s 
-                         in runState (f v) s' )
+-- ghci> runState pure' 3 == runState (fmap (+5) get) 3
+-- True
 
 
-  return g = S (\s -> (s,g)) 
+-- the functor definition only applies to the 'a' type of 'State s a'
+-- so no functor methods can directly access the 's' type.
+-- the pure' equivalence emerges because 'return' is equivalent 
+-- to 'pure' and 'fmap' is really:
 
-
-increment = get >>= \n -> put (n+1)   -- ghci> runState increment 5
-                                      -- (6, ())
-
-pure'  = get >>= \n -> return (n+5)   -- ghci> runState pure' 2
-                                      -- (2,3) 
-
--- ghci> runState pure' 3 == runState (fmap (+5) get) 3   >> True
+-- ' ap . pure ' 
