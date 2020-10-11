37e6b8009668bd58858852b35a0dbf660b95adfb
varreli <varreli@gmail.com>
2020-10-10 21:58:33 -0700

    Add brian.hs, zoom session

---
+++ folds/brian.hs
@@ -0,0 +1,71 @@
+-- flip is required because the types need to 
+-- agree with (:) . First, foldl extracts 10 :
+
+-- foldl (flip (:)) [1,2,3] [10,11]
+-- is the same as:
+(:) 10 [1,2,3]
+(:) 11 [10,1,2,3]
+
+[11,10,1,2,3]
+
+-- Unification of foldl and (flip (:))   
+
+-- (:) has this pattern 
+a -> [a] -> [a] 
+-- featured in our exercise: 1:[2] -> [1,2]
+
+-- the correct pattern for foldl is (flip (:)) 
+-- (compare with line 14) :
+
+[a] -> a -> [a]
+
+-- we then substitute ([a] -> a -> [a]) for the
+-- (b -> a -> b) here:
+
+foldl :: (b -> a -> b) -> b -> [a] -> b
+
+-- can you see how they are the same? 
+-- ([a] -> a -> [a])
+-- ( b  -> a ->  b )
+
+-- (flip (:)) is satisfied, and the remainder of the 
+-- signature is the last 3 elements of foldl, 
+-- b -> [a] -> b , altered by ghci to indicate the 
+-- 'foldable' t :
+
+foldl (flip (:)) :: Foldable t => [a] -> t a -> [a]
+
+-- so now substitute in our relations of those to t: 
+-- (b ~ [t], a ~ t):  
+
+foldl :: ([t] -> t -> [t]) -> [t] -> [t] -> [t]
+foldl (flip (:)) :: [t] -> [t] -> [t]
+
+-- b unifies with [t], a unifies with t 
+
+-- (flip (:)) is handed the current base (b) and a list 
+-- element (a) and produces a new base (b). You provide
+-- an initial base or 'seed' (misnamed accumulator), 
+-- and a list (Foldable).  
+
+
+-- examples:
+
+λ > foldl (/) 64 [1,2,4] 
+λ > scanl (/) 64 [1,4,2] 
+
+λ > foldl (-) 30 [1,2] 
+λ > scanl (/) 30 [1,2] 
+
+-- the first parameter (64) is the base, or seed.
+-- It isn't updated, its just replaced with a new value.
+-- Updating is mutation but this parameter is immutable.
+
+
+-- Prelude > foldl (\x y -> 2*x + y) 4 [1,2,3]
+-- 43
+
+foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b
+
+foldl f z [ ] = z
+foldl f z (x : xs) = foldl f (f z x) xs
--- monads/kleisli.hs
+++ monads/kleisli.hs
@@ -5,17 +5,23 @@ import Control.Monad
 (|>) :: (a -> b) -> (b -> c) -> (a -> c)
 (|>) = flip (.)
 
+-- Left-to-right Kleisli composition of monads:
 
 (!>) :: Monad m => (a -> m b) -> (b -> m c) -> (a -> m c)
 (!>) f g = f |> fmap g |> join 
+
+(>=>) :: Monad m => (a -> m b) -> (b -> m c) -> (a -> m c)
+f >=> g = \x -> f x >>= g
 
 
 
 vv = map toLower |> map toUpper
 ww = (|>) (map toLower) (map toUpper) 
 
+
 xx = (map toLower) . (map toUpper)
 yy = (map toUpper) . (map toLower)
+
 
 qq = (/2) . (*100)
 uu = (flip (.)) (/2) (*100) 
@@ -25,6 +31,10 @@ uu = (flip (.)) (/2) (*100)
 average :: [Double] -> Double
 
 average n = sum n / fromIntegral (length n)
+
+
+-- note section on haskell (f# question) :
+-- https://stackoverflow.com/questions/30110964/what-f-sorcery-is-this 
 
 -- review now! :
 -- https://www.schoolofhaskell.com/user/Lkey/kleisli
