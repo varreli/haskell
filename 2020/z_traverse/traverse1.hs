ee71adcd21874e9e182fb74e1eaa80eb62dc0450
John Werner <varreli@gmail.com>
2020-10-27 23:40:02 -0700

    Synch

--- 2020/z_traverse/traverse1.hs
+++
@@ -1,9 +0,0 @@
-data Tree a = Con a | Add (Tree a) (Tree a)
-    deriving (Eq, Show)
-
-instance Functor Tree where
-    fmap f (Con a) = Con (f a)
-    fmap f (Add x y) = Add (fmap f x) (fmap f y)
-
-
-https://blog.plover.com/prog/haskell/traversable.html
