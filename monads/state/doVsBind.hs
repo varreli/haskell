60b371fe6bc7b0d984c4d1c8a8986ebc9294f179
John Werner <varreli@gmail.com>
2020-11-27 21:35:59 -0800

    add parseMaybe.hs to 86_parse

--- monads/state/doVsBind.hs
+++ monads/state/doVsBind.hs
@@ -1,18 +1,10 @@
 import HuttSt
 import Prelude hiding (read)
 
--- instance Monad ST where
--- (>>=) :: m a -> (a -> m b) -> m b
---   return a = S $ \s -> (a,s)
---   S st >>= f = 
---     S $ \s -> let (a, s') = st s
---               in unwrap (f a) s'  
-
 binder :: ST (Either String Int)                        
 binder = read >>= \x -> write (x+1) >>= \_ -> read >>=   
          \x -> return (safeDiv (x*2) x)                   
 
-ee = unwrap binder (-1)
 -- we bind the result of read to x; it's the a in the first place of the 
 -- tuple (a,s) . The a is what the Monad instance acts upon and >>= is 
 -- from Monad. read puts the same value into both sides of the tuple (s,s) .
@@ -26,9 +18,22 @@ ee = unwrap binder (-1)
 -- is equivalent to:
 -- x >>= \_ -> y
 
+-----------------------------------------------------------------------------
+
 binderr :: ST (Either String Int)
 binderr =
         do x <- read
            write (x+1)
            x <- read
            return (safeDiv (x*2) x)
+
+-- note how mk_action4 works on x1 and x3 :
+
+-- binder = action1 >>= (\x1 -> action2 >>= (\_ -> action3 >>= 
+--          (\x3 -> mk_action4 x1 x3 ))
+
+-- binderr = do { x1 <- action1
+--              ;       action2
+--              ; x3 <- action3
+--              ; mk_action4 x1 x3 }
+
