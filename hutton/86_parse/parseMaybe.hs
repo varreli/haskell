876ea799e03ccf3c1f9463355a3380183846d822
John Werner <varreli@gmail.com>
2020-11-28 21:38:59 -0800

    parser

--- hutton/86_parse/parseMaybe.hs
+++ hutton/86_parse/parseMaybe.hs
@@ -1,12 +1,38 @@
 import Control.Applicative
 import Data.Char
-
 newtype Parser a = P (String -> [(a,String)]) 
 
 parse :: Parser a -> String -> [(a,String)]
 parse (P p) inp = p inp
 
-item :: Parser Char
+item :: Parser Char 
 item = P (\inp -> case inp of
                     [] -> []
                     (x:xs) -> [(x,xs)])
+---------------------------------------------------------------
+instance Functor Parser where
+-- fmap :: (a -> b) -> Parser a -> Parser b
+   fmap g p = P (\inp -> case
+                           parse p inp of
+                           [] -> []
+                           [(v,out)] -> [(g v, out)])
+
+upp = parse (fmap toUpper item) "xyz"
+low = parse (fmap toLower item) ""
+---------------------------------------------------------------
+instance Applicative Parser where
+-- pure :: a -> Parser a
+   pure v = P (\inp -> [(v,inp)])
+   pg <*> px = P (\inp -> case
+                            parse pg inp of
+                            [] -> []
+                            [(g,out)] -> parse (fmap g px) out)
+
+ff = parse (pure 88) "abc"
+
+three :: Parser (Char,Char) 
+three = pure g <*> item <*> item <*> item 
+    where g x y z = (x,z)
+
+ii = parse three "wqxijk"
+----------------------------------------------------------------
