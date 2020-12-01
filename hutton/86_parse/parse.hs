96d122d5ea4d5ccd91b82904d0684c78e26ab0c3
varreli <varreli@gmail.com>
2020-11-30 20:45:37 -0800

    Add Alternative and boolean predicates

--- hutton/86_parse/parse.hs
+++ hutton/86_parse/parse.hs
@@ -1,6 +1,7 @@
 import Control.Applicative
 import Data.Char
-newtype Parser a = P (String -> [(a,String)]) 
+
+newtype Parser a = P (String -> [(a,String)])  
 
 parse :: Parser a -> String -> [(a,String)]
 parse (P p) inp = p inp
@@ -22,8 +23,8 @@ low = parse (fmap toLower item) ""
 ---------------------------------------------------------------
 instance Applicative Parser where
 -- pure :: a -> Parser a
-   pure v = P (\inp -> [(v,inp)])
-   pg <*> px = P (\inp -> case
+  pure v = P (\inp -> [(v,inp)])
+  pg <*> px = P (\inp -> case
                             parse pg inp of
                             [] -> []
                             [(g,out)] -> parse (fmap g px) out)
@@ -35,8 +36,8 @@ three = pure g <*> item <*> item <*> item
     where g x y z = (x,z)
 
 ii = parse three "wqxijk"
-----------------------------------------------------------------
-
+-- ----------------------------------------------------------------
+-- 
 instance Monad Parser where
 -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
   p >>= ff = P (\inp -> case
@@ -47,4 +48,32 @@ instance Monad Parser where
 three' = do x <- item 
             item 
             z <- item 
-            return (x,z) 
+            return (x,z)  -- return == pure : (injects the vals) 
+
+instance Alternative Parser where
+-- empty = Nothing
+  empty = P (\inp -> [])
+-- (<|>) :: Parser a -> Parser -> a -> Parser a
+  p <|> q = P (\inp -> case parse p inp of
+                         [] -> parse q inp
+                         [(v,out)] -> [(v,out)])
+
+-- ghci> parse (item <|> return 'd') "abcd"
+-- [('a',"bcd")]
+-- ghci> parse (empty <|> return 'd') "abcd"
+-- [('d',"abcd")]
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = do x <- item
+               if p x then return x else empty
+
+-- ghci> parse (satisfy isDigit) "3rst"
+-- [('3',"rst")]
+
+alphanum = parse (satisfy isAlphaNum)
+-- ghci> alphanum "*45"
+-- []
+
+char :: Char -> Parser Char
+char x = satisfy (== x) 
+match letter = parse (char letter)
