c9f1ad2703f343913452739554dfc322bd0533a3
John Werner <varreli@gmail.com>
2020-12-14 16:50:05 -0800

    Synch

--- hutton/86_parse/currentTsoding.hs
+++ hutton/86_parse/currentTsoding.hs
@@ -1,4 +1,5 @@
 module Main where
+import Data.Char
 
 -- definition of the AST:
 
@@ -19,16 +20,34 @@ data JsonValue = JsonNull
 newtype Parser a = Parser 
                  { runParser :: String -> Maybe (a, String) 
                  }
+
+instance Functor Parser where
+    fmap f (Parser p) = Parser $ \input -> do         -- p is the func that consumes input
+                          (x, input') <- p input      -- this proves Parser is a functor
+                          Just (f x, input)           -- (also we can use do since its a Monad)
+
+-- ghci> runParser (fmap ord $ charP 'n') "nice"
+-- Just (110, "ice")
+
+-- now to prove Parser is Applicative:
+
+instance Applicative Parser where
+    pure x = \input -> Just (x, input)
+    (<*>) 
 jsonNull :: Parser JsonValue
 jsonNull = undefined
 ------------------------------------------------------------------------------------------------- 
 charP :: Char -> Parser Char       -- helper function to parse a single char, (parameter Char ->)
-charP c = Parser f       -- and supplies a Parser to parse that char
+charP c = Parser f                 -- and supplies a Parser to parse that char
             where 
               f (y:ys) 
                 | y == c = Just (c, ys)
                 | otherwise = Nothing  
-                                      
+              f [] = Nothing                  -- see tsoding 27:45 re: print/show error
+
+-- stringP :: String -> Parser String
+-- stringP = input = sequenceA $ map charP input
+                       
 
 -- The above is a parser capable of parsing a single char, therefore a sequence of chars --------
 
