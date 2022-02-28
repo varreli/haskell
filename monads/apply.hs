37f242c6d84f9246d37069089fe4cb0166450dee
varreli <varreli@gmail.com>
2022-02-28 15:03:58 -0800

    Synch

--- monads/apply.hs
+++ monads/apply.hs
@@ -15,7 +15,11 @@ applly f g = f >>= \ff -> g >>= \gg -> return ff (gg)
 
 -- (apply should work) .. following are the same:
 
+ll = liftM2 (/)  (realToFrac . sum) genericLength [4,5,6]
 mm = liftM2 ((/) . realToFrac) sum genericLength [4,5,6]
-oo = liftM2 ((/) . realToFrac) (foldr (+) 0) genericLength [10,20]
+-- these two are identical.
+
+
+nn = liftM2 ((/) . realToFrac) (foldr (+) 0) genericLength [10,20]
 
 -- https://stackoverflow.com/questions/33454650/understanding-ap-in-a-point-free-function-in-haskell
