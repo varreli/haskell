8c7d7978a2c17a1ec6f617e7d8060a22be2dfecc
varreli <varreli@gmail.com>
2022-05-01 21:20:42 -0700

    Synch

--- monads/automata.hs
+++ monads/automata.hs
@@ -22,10 +22,9 @@ class Functor w => Comonad w where
     x =>> f = fmap f (cojoin x)
 
 instance Comonad U where
-   cojoin a = U (tail $ iterate left a) a (tail $ iterate right a)
+   cojoin a = U (tail $ take 3 (iterate left a)) a (tail $ take 3 (iterate right a))
    coreturn (U _ b _) = b
 
--- rule (U (a:_) b (c:_)) = not (a && b && not c || (a==b))
+rule (U (a:_) b (c:_)) = not (a && b && not c || (a==b))
 
 -- http://blog.sigfpe.com/2006/12/evaluating-cellular-automata-is.html
-
