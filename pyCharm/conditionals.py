56ab0aec10c5a580d9632d8f63f02325b02de25a
varreli <varreli@gmail.com>
2022-07-26 20:28:04 -0700

    remove pyCharm

--- pyCharm/conditionals.py
+++
@@ -1,7 +0,0 @@
-a = 15
-
-if a < 9:
-        print('a is less than 9')
-
-else: 
-        print ('a is >= 9')
--- pyCharm/linkedProj.py
+++
@@ -1,8 +0,0 @@
-def outer_function(a, b):
-    def inner_function(c, d):
-        return c + d
-
-    return inner_function(a, b)
-
-result = outer_function(5, 10)
-print(result)
